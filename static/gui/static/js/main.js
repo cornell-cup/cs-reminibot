db6f12d611fd27d138d58214afcddd7cf3ab1569
ningning621 <ningning621@gmail.com>
2018-04-14 14:22:16 -0400

    gui without react libs lol

--- static/gui/static/js/main.js
+++ static/gui/static/js/main.js
@@ -1,14 +1,119 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class HelloWorld extends React.Component{
+class AddBot extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bot_name: "",
+            bot_list: []
+        };
+
+        this.updateInputValue = this.updateInputValue.bind(this);
+        this.buttonPress = this.buttonPress.bind(this);
+        this.selectBot = this.selectBot.bind(this);
+        this.buttonMapPress = this.buttonMapPress.bind(this);
+    }
+
+    updateInputValue(event) {
+        console.log("update input value");
+        this.state.bot_name = event.target.value;
+    }
+
+    buttonPress(event) {
+        console.log("button press");
+        var li = this.state.bot_list;
+        li.push(this.state.bot_name);
+        this.setState({bot_list: li});
+    }
+
+    selectBot(event) {
+        console.log("select bot");
+        console.log(event.target.value);
+    }
+
+    buttonMapPress(event) {
+        console.log("button map press");
+        console.log(event.target.id);
+    }
+
+    render() {
+        var styles = {
+            Select: {
+                marginLeft: '10px',
+                marginRight: '10px'
+            },
+            Button: {
+                marginLeft: '10px',
+                marginRight: '10px',
+                float: 'left'
+            }
+        }
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                            <div>Bot Name or whatever Leo wants:</div>
+                            <form>
+                                <label>
+                                    <input type="text" name="bot_name" onChange={evt => this.updateInputValue(evt)}/>
+                                </label>
+                            </form>
+                            </td>
+                            <td><button style={styles.Button} onClick={this.buttonPress}>Add Bot</button></td>
+                        </tr>
+                        <tr>
+                        <td><div> Bot List: </div></td>
+                        <td><select style={styles.Select} onChange={this.selectBot}>
+                            {
+                                this.state.bot_list.map(function(bot_name, idx){
+                                    return <option
+                                                key={idx}
+                                                value={bot_name}>
+                                           {bot_name}
+                                           </option>
+                                })
+                            }
+                        </select></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div id="button map">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td><button className="btn" id="f" onClick={this.buttonMapPress}>forward</button></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><button className="btn" id="l" onClick={this.buttonMapPress}>left</button></td>
+                            <td><button className="btn" id="s" onClick={this.buttonMapPress}>stop</button></td>
+                            <td><button className="btn" id="r" onClick={this.buttonMapPress}>right</button></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><button className="btn" id="b" onClick={this.buttonMapPress}>backward</button></td>
+                            <td></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+class ClientGUI extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
             name: ""
         }
     }
-    
+
     updateInputValue(event) {
         this.setState({name: event.target.value});
     }
@@ -20,10 +125,11 @@ class HelloWorld extends React.Component{
                 <div>{this.props.userid}</div>
                 <form>
                     <label>
-                        Name: 
+                        Name:
                         <input type="text" name="name" onChange={evt => this.updateInputValue(evt)}/>
                     </label>
                 </form>
+                <AddBot/>
             </div>
         )
     }
@@ -31,5 +137,5 @@ class HelloWorld extends React.Component{
 
 let root = document.getElementById('root');
 ReactDOM.render(
-    <HelloWorld />, root
+    <ClientGUI />, root
 );
