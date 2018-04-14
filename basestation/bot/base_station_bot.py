9f46c6d35a037ab9e1d78c26cac63cebf3da5c08
Ashley Yang <ay322@cornell.edu>
2018-04-14 13:13:05 -0400

    bot

--- basestation/bot/base_station_bot.py
+++ basestation/bot/base_station_bot.py
@@ -1,7 +1,9 @@
 """
 Base Station Bot.
 """
-from connection.tcp_connection import TCPConnection
+from basestation.connection.tcp_connection import TCPConnection
+import threading
+from basestation.util.exception_handling import *
 
 class BaseStationBot:
     def __init__(self, bot_id, ip, port=10000):
@@ -12,7 +14,7 @@ class BaseStationBot:
         self.tcp_connection = TCPConnection(ip, port=port)
 
     def __del__(self):
-        pass
+        self.tcp_connection.destroy()
 
     def get_id(self):
         return self.id
@@ -22,3 +24,34 @@ class BaseStationBot:
 
     def is_active(self):
         return self.tcp_connection.is_connection_active()
+
+    def sendKV(self, key, value):
+        return self.tcp_connection.sendKV(key, value)
+
+    class TCPListener(threading.Thread):
+        def __init__(self, t):
+            super().__init__()
+            self.tcp_connection = t
+
+        def run(self):
+            try:
+                while True:
+                    if self.tcp_connection.is_connection_active():
+                        msg = self.tcp_connection.receive()
+                        if msg is not None:
+                            self.tcp_parse_incoming(msg)
+            except RuntimeError as e:
+                msg = "TCP receive failed"
+                log_exn_info(e, msg = msg)
+
+        def tcp_parse_incoming(self, data):
+            start = data.find("<<<<")
+            end = data.find(">>>>")
+            comma = data.find(",")
+            if start != -1 and comma != -1 and end != -1:
+                key = data[start + 4: comma]
+                value = data[comma+1: end]
+                self.tcp_act_on_incoming(key, value)
+            return True
+        def tcp_act_on_incoming(self, key, value):
+            print("key: " + key + ", value:" + value)
