35d7ddd41d9396c0fb9dedf1fc12b9fc148db908
Leo Tang <lt342@cornell.edu>
2018-04-16 22:49:08 -0400

    Fixed import and added bot name

--- basestation/bot/base_station_bot.py
+++ basestation/bot/base_station_bot.py
@@ -4,10 +4,14 @@ Extends Bot class.
 """
 
 class BaseStationBot(object):
-    def __init__(self, bot_id):
+    def __init__(self, bot_id, bot_name):
         self.id = bot_id
+        self.name = bot_name
        
     def get_id(self):
     	return self.id
+
+    def get_name(self):
+    	return self.name
 
 
