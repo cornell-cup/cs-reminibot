eda9e19437d254ddbb910f3ce35464beb188bdb1
Leo Tang <lt342@cornell.edu>
2018-04-17 16:17:41 -0400

    Added has_bot fnction

--- basestation/session/session.py
+++ basestation/session/session.py
@@ -6,15 +6,18 @@ run by the user.
 class Session:
     def __init__(self, session_id):
         self.session_id = session_id
-        self.bots = {}
+        self.bots = set([])
         self.playground = None
 
     def get_session_id(self):
         return self.session_id
 
     def add_bot_id_to_session(self, bot_id):
-        self.bots.append(bot)
+        self.bots.add(bot_id)
         return True
+
+    def has_bot(self, bot_id):
+        return bot_id in self.bots
 
     def get_session_bots(self):
         return self.bots
