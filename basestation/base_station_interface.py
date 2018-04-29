60010feaf4861a664be300abf42f28a3108c2e25
ningning621 <ningning621@gmail.com>
2018-04-28 22:18:23 -0400

    changes

--- basestation/base_station_interface.py
+++ basestation/base_station_interface.py
@@ -105,7 +105,7 @@ class ClientHandler(tornado.web.RequestHandler):
             self.write(json.dumps(self.base_station.get_active_bots_names()).encode())
         elif key == "DISCONNECTBOT":
             bot_name = data['bot']
-            print(bot_name)
+            print("bot name: " + bot_name)
             bot_id = self.base_station.bot_name_to_bot_id(bot_name)
             print(self.base_station.get_active_bots_names)
             self.base_station.remove_bot_from_session(session_id, bot_id)
