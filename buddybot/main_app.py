86bd2ba93c50acefc1c570b6ae2c2f6875d12106
Viren <v.virenshah@gmail.com>
2019-04-11 23:55:05 +0000

    Working on feedback loop

--- buddybot/main_app.py
+++ buddybot/main_app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, Response, request
 from drive import Drive
+import threading
+import time
 app = Flask(__name__)
 dr = Drive()
 
-@app.route("/move", methods=["POST"])
+@app.route('/move', methods=['POST'])
 def move():
     content = request.json
     direction = content['direction']
@@ -11,13 +13,43 @@ def move():
     drive_time = 1 # delete this after we add drive_time field to app post request
     if direction == 'forward':
         dr.forward()
+	update_response('forward')
     elif direction == 'backward':
         dr.backward(drive_time)
+	update_response('backward')
     elif direction == 'stop':
         dr.stop()
-	
+	update_response('stop')
+
     print(direction)
     return direction
 
-if __name__ == "__main__":
+@app.route('/response')
+def respond():
+    return response_str
+
+
+lock = threading.Lock()    
+response_str = ''
+request_time = 0
+
+def update_response(string):
+    lock.acquire()
+    response_str = string
+    response_time = time.time() * 1000000
+    lock.release()
+
+# Ensure that there is constant communication from the app to the buddybot
+# If no request has been sent in the last second then tell buddybot to stop
+def reset():
+    while(True):
+	time.sleep(0.25)
+        current_time = time.time() * 1000000
+	lock.acquire()
+        if current_time - request_time >= 1.0:
+	    response_str = ''
+    
+	
+
+if __name__ == '__main__':
     app.run(host = '0.0.0.0')
