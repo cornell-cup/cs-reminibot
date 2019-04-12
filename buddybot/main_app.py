15563ad1153e4741729282b700d5e165f22debb7
Viren Shah <vvs24@cornell.edu>
2019-04-12 15:52:19 -0400

    Finished feedback loop on buddybot side

--- buddybot/main_app.py
+++ buddybot/main_app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, Response, request
-from drive import Drive
+#from drive import Drive
 import threading
 import time
 app = Flask(__name__)
-dr = Drive()
+#dr = Drive()
 
 @app.route('/move', methods=['POST'])
 def move():
@@ -12,44 +12,52 @@ def move():
     # drive_time = content['drive_time']
     drive_time = 1 # delete this after we add drive_time field to app post request
     if direction == 'forward':
-        dr.forward()
-	update_response('forward')
+        #dr.forward()
+        print("forward")
+        update_response('forward')
     elif direction == 'backward':
-        dr.backward(drive_time)
-	update_response('backward')
+        #dr.backward(drive_time)
+        print("backward")
+        update_response('backward')
     elif direction == 'stop':
-        dr.stop()
-	update_response('stop')
+        print("stop")
+        # dr.stop()
+        update_response('stop')
 
-    print(direction)
+   # print(direction)
     return direction
 
 @app.route('/response')
 def respond():
     return response_str
 
-
-lock = threading.Lock()    
-response_str = ''
-request_time = 0
-
 def update_response(string):
+    global response_str
+    global request_time
     lock.acquire()
     response_str = string
-    response_time = time.time() * 1000000
+    request_time = time.time()
     lock.release()
 
 # Ensure that there is constant communication from the app to the buddybot
 # If no request has been sent in the last second then tell buddybot to stop
 def reset():
+    global response_str
     while(True):
-	time.sleep(0.25)
-        current_time = time.time() * 1000000
-	lock.acquire()
-        if current_time - request_time >= 1.0:
-	    response_str = ''
-    
+        time.sleep(0.25)
+        current_time = time.time()
+        lock.acquire()
+ #       print('Response String: {} Time since response: {}'.format(response_str, current_time-request_time))
+        if response_str != 'stop' and current_time - request_time >= 1.0:
+            print('stop')
+            response_str = 'stop'
+        lock.release()
 	
 
 if __name__ == '__main__':
+    lock = threading.Lock()    
+    response_str = 'stop'
+    request_time = 0
+    reset_thread = threading.Thread(target=reset)
+    reset_thread.start()
     app.run(host = '0.0.0.0')
