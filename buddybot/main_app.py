86394761d2f407bbeafe8da72fc17f320847c06c
Andy <azhu99@gmail.com>
2019-04-25 18:08:05 -0400

    Add documentation for buddy bot server

--- buddybot/main_app.py
+++ buddybot/main_app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, Response, request
-from drive import Drive
+"""App to run on the buddybot that processes move requests."""
+
+
 import threading
 import time
+from flask import Flask, request
+from drive import Drive
+
 app = Flask(__name__)
 dr = Drive()
 
+
+@app.route('/check', methods=['GET'])
+def check():
+    """Verification method for the GUI app to ensure user have entered a valid IP"""
+    return "OK!"
+
+
 @app.route('/move', methods=['POST'])
 def move():
+    """Moves the buddybot."""
     content = request.json
     direction = content['direction']
     if direction == 'forward':
@@ -33,12 +45,12 @@ def move():
     return direction
 
 
-
-lock = threading.Lock()    
+lock = threading.Lock()
 response_str = 'stop'
 request_time = 0
 
 def update_response(string):
+    """Updates the time and name of the current movement request."""
     global response_str
     global request_time
     lock.acquire()
@@ -46,11 +58,12 @@ def update_response(string):
     request_time = time.time()
     lock.release()
 
-# Ensure that there is constant communication from the app to the buddybot
-# If no request has been sent in the last second then tell buddybot to stop
+
 def reset():
+    """Ensure that there is constant communication from the app to the buddybot
+    If no request has been sent in the last second then tell buddybot to stop"""
     global response_str
-    while(True):
+    while True:
         time.sleep(0.05)
         current_time = time.time()
         lock.acquire()
@@ -60,7 +73,7 @@ def reset():
             print('stop')
             response_str = 'stop'
         lock.release()
-	
+
 
 if __name__ == '__main__':
     reset_thread = threading.Thread(target=reset)
