0198b4c155b84fef37646667f20740807e9c1e41
Andy Zhu <azhu99@gmail.com>
2019-09-07 13:48:28 -0400

    Fix imports for client gui script component

--- static/gui/static/js/components/Scripts.js
+++ static/gui/static/js/components/Scripts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import Python from './Python';
+
 export default class Scripts extends React.Component {
   constructor(props) {
       super(props);
