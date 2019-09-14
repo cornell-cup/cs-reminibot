b32350a1a6bd3b8bc4d6ca3aa5f15dd790ed22cf
Viren Shah <vvs24@cornell.edu>
2019-09-14 13:49:51 -0400

    Added the run_BS.sh to buddybot

---
+++ run_BS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "================= MINIBOT CLIENT GUI ================="
+cd static
+cd gui
+npm run webpack
+echo "================= MINIBOT BASESTATION GUI ================="
+cd ..
+cd basestation_gui
+npm run webpack
+echo "=========== WEBPACK SETUP COMPLETE ============"
+cd ..
+cd ..
+cd basestation
+echo "=========== STARTING BASESTATION ==============="
+python3 base_station_interface.py
