d6e3b4741feab889c40d24efe1ceacad376d7ca6
Viren Shah <vvs24@cornell.edu>
2019-09-28 09:51:27 -0400

    Renamed client to heartbeat

--- minibot/client.py
+++ minibot/client.py
@@ -9,23 +9,27 @@ import time
 
 # Create a UDP socket
 sock = socket(AF_INET, SOCK_DGRAM)
+# can bind to the same port using the same ip address
 sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+# can broadcast messages to all
 sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
+# address 255.255.255.255 allows you to broadcast to all 
+# ip addresses on the network
 server_address = ('255.255.255.255', 9434)
 message = 'i_am_a_minibot'
 
 
-def start_base_station_communication(ip_address):
+def start_base_station_heartbeat(ip_address):
     # Define broadcasting address and message
     server_address = (ip_address, 5001)
-    message = 'Hello, I am a minibot!'
+    heartbeat_message = 'Hello, I am a minibot!'
         
         # Send message and resend every 9 seconds
     while True:
         try:
 		    # Send data
-            print('sending broadcast: "%s"' % message)
+            print('sending broadcast: "%s"' % heartbeat_message)
             sent = sock.sendto(message.encode(), server_address)
         except Exception as err:
             print(err)
@@ -34,6 +38,8 @@ def start_base_station_communication(ip_address):
 
 try:
     server_ip = None
+
+    # continuously try to connect to the base station
     while True:
         # Send data
         print('sending: ' + message)
@@ -50,12 +56,11 @@ try:
 	        print('Verification failed')
 	        print('Trying again...')
 
-    base_station_thread = Thread(target=start_base_station_communication, args=(server_ip,)) 
+    base_station_thread = Thread(target=start_base_station_heartbeat, args=(server_ip,)) 
     base_station_thread.start()
     tcp_instance = TCP()
     while True:
-        tcp_cmd = tcp_instance.get_command()
-        parse_command(tcp_cmd, bot, tcp_instance)
+        # ned to insert parse commands from base station here
         time.sleep(0.01)
 	
 	
