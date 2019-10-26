e9b0f316bf02f52cd62226c17060a1a2254dc8d1
tl676 <54723399+tl676@users.noreply.github.com>
2019-10-26 17:39:30 -0400

    aesthetic changes

--- static/gui/static/js/components/blockly.js
+++ static/gui/static/js/components/blockly.js
@@ -90,111 +90,159 @@ export default class MinibotBlockly extends React.Component {
         //document.getElementById('blockly').value = window.Blockly.Python.workspaceToCode(this.workspace);
 
         console.log(blockly.value);
-        console.log('sent script');
-    })
-    .catch(function (error) {
-        console.warn(error);
-    });
-  }
+    }
+
+    /* DOWNLOAD FUNCTION
+         Allows users to download raw code as a file. Users must
+         manually input file name and file ext.
+      */
+    download(event) {
+        event.preventDefault();
+        var element = document.createElement('a');
+        var xmlDom = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace());
+        var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+        var filename = this.state.blockly_filename;
+        if (filename.substring(filename.length - 4) != '.xml') {
+            filename += '.xml';
+        }
+        element.setAttribute(
+            'href',
+            'data:text/plain;charset=utf-8,' + encodeURIComponent(xmlText)
+        );
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    upload(event) {
+        var _this = this;
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            _this.state.data = event.target.result;
+            document.getElementById('data').value = event.target.result;
+        };
+        reader.readAsText(file);
+    }
+
+    /* Target function for the button "Run". Send python code
+       corresponding to blockly to backend. */
+    run_blockly(event) {
+        console.log(this.props.bot_name, "run_blockly");
+        axios({
+            method: 'POST',
+            url: '/start',
+            data: JSON.stringify({
+                key: 'SCRIPTS',
+                value: blockly.value,
+                bot_name: this.props.bot_name
+            }),
+        })
+            .then(function (response) {
+                console.log(blockly.value);
+                console.log('sent script');
+            })
+            .catch(function (error) {
+                console.warn(error);
+            });
+    }
 
 
-  /* Target function for the button "Run Code". Send python code
-     in the editing box to backend. */
-  run_script(event){
-    console.log(name);
-    axios({
-        method:'POST',
-        url:'/start',
-        data: JSON.stringify({
-            key: 'SCRIPTS',
-            value: this.state.data,
-            bot_name: name
-        }),
-    })
-    .then(function(response) {
-        // console.log(axois.data.value);
-        console.log('sent script');
-    })
-    .catch(function (error) {
-        console.warn(error);
-    });
-}
+    /* Target function for the button "Run Code". Send python code
+       in the editing box to backend. */
+    run_script(event) {
+        console.log(this.props.bot_name, "run_script");
+        axios({
+            method: 'POST',
+            url: '/start',
+            data: JSON.stringify({
+                key: 'SCRIPTS',
+                value: this.state.data,
+                bot_name: this.props.bot_name
+            }),
+        })
+            .then(function (response) {
+                // console.log(axois.data.value);
+                console.log('sent script');
+            })
+            .catch(function (error) {
+                console.warn(error);
+            });
+    }
 
-  /* Target function for the button "Cope Code". Set the text
-     in the editing box according to blockly. */
-  copy(event){
-    document.getElementById("textarea").value = data.innerText;
-    this.setState({data: data.innerText});
-  }
+    /* Target function for the button "Cope Code". Set the text
+       in the editing box according to blockly. */
+    copy(event) {
+        document.getElementById("textarea").value = data.innerText;
+        this.setState({ data: data.innerText });
+    }
 
-  loadFileAsBlocks(event) {
-    var xmlToLoad = document.getElementById('blockUpload').files[0];
-    var xmlReader = new FileReader();
-    xmlReader.onload = function(event) {
-      var textFromFileLoaded = event.target.result;
-      console.log(textFromFileLoaded);
-      var dom = Blockly.Xml.textToDom(textFromFileLoaded);
-      Blockly.getMainWorkspace().clear();
-      Blockly.Xml.domToWorkspace(dom, Blockly.getMainWorkspace());
-    };
+    loadFileAsBlocks(event) {
+        var xmlToLoad = document.getElementById('blockUpload').files[0];
+        var xmlReader = new FileReader();
+        xmlReader.onload = function (event) {
+            var textFromFileLoaded = event.target.result;
+            console.log(textFromFileLoaded);
+            var dom = Blockly.Xml.textToDom(textFromFileLoaded);
+            Blockly.getMainWorkspace().clear();
+            Blockly.Xml.domToWorkspace(dom, Blockly.getMainWorkspace());
+        };
 
-    xmlReader.readAsText(xmlToLoad, 'UTF-8');
-  }
+        xmlReader.readAsText(xmlToLoad, 'UTF-8');
+    }
 
-  render() {
-    var blocklyStyle = { height: '67vh'};
-    var marginStyle = {marginLeft: '10px'};
-    var dataStyle = {align: 'right'};
-
-    return (
-      <div id="blockyContainer" style = {marginStyle} className="row">
-      <div id="blockly" className="box" className="col-md-7">
-        <div id="blocklyDiv" style={blocklyStyle} align="left">
-          <p id ="title"><b>Blockly </b> </p>
+    render() {
+        var blocklyStyle = { margin: '0', height: '67vh' };
+        return (
+            <div id="blockyContainer" className="row">
+                <div id="blockly" className="box" className="col-md-7">
+                    <div id="blocklyDiv" style={blocklyStyle} align="left">
+                        Blockly
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        Blockly File Name:{' '}
-        <input
-          type="text"
-          name="blockly_filename"
-          value={this.state.blockly_filename}
-          onChange={this.handleInputChange}
-        />
-        <button id="blocklySubmit" onClick={this.download}>
-          Download
+                    <br />
+                    Blockly File Name:{' '}
+                    <input
+                        type="text"
+                        name="blockly_filename"
+                        value={this.state.blockly_filename}
+                        onChange={this.handleInputChange}
+                    />
+                    <button id="blocklySubmit" onClick={this.download}>
+                        Download
         </button>&nbsp;&nbsp;
         <button id="blockyRun" onClick={this.run_blockly}>
                         Run
         </button>
-        <form>
-          <input
-            type="file"
-            id="blockUpload"
-            multiplesize="1"
-            accept=".xml"
-            onChange={this.loadFileAsBlocks}
-          />
-        </form>
-        <br />
+                    <form>
+                        <input
+                            type="file"
+                            id="blockUpload"
+                            multiplesize="1"
+                            accept=".xml"
+                            onChange={this.loadFileAsBlocks}
+                        />
+                    </form>
+                    <br />
 
-      <div id="Python">
-      <p id ="title"> <b>Python </b> </p>
-      <div> File name:  <input type="text" name="filename" value={this.state.filename} onChange={this.handleFileNameChange}/> </div>
-      <div> <textarea id = "textarea" onChange={this.handleScriptChange} /></div>
-      <button id="submit" onClick={this.download}>Download</button>
-      <button id="run" onClick={this.run_script}>Run Code</button>
-      <button id="save" onClick={this.save}>Save Code</button>
-      <button id="copy" onClick={this.copy}>Copy Code</button>
-      <div>{this.state.data}</div>
-      <div>{this.state.filename}</div>
-      </div>
+                    <div id="Python">
+                        Python
+      <div> File name:  <input type="text" name="filename" value={this.state.filename} onChange={this.handleFileNameChange} /> </div>
+                        <div> <textarea id="textarea" onChange={this.handleScriptChange} /></div>
+                        <button id="submit" onClick={this.download}>Download</button>
+                        <button id="run" onClick={this.run_script}>Run Code</button>
+                        <button id="save" onClick={this.save}>Save Code</button>
+                        <button id="copy" onClick={this.copy}>Copy Code</button>
+                        <div>{this.state.data}</div>
+                        <div>{this.state.filename}</div>
+                    </div>
 
-      </div>
-      <div id="data" style = {dataStyle} className="col-md-5"></div>
-      </div>
-    );
-  }
+                </div>
+                <div id="data" className="col-md-5">
+                </div>
+
+            </div>
+        );
+    }
 }
