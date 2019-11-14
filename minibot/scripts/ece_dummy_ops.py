9393692e23caadd643b841d92a45ef5c72463f92
Viren Shah <vvs24@cornell.edu>
2019-11-13 20:52:26 -0500

    Changes to PiArduino.py

--- minibot/scripts/ece_dummy_ops.py
+++ minibot/scripts/ece_dummy_ops.py
@@ -1,10 +1,9 @@
+#import binascii
+#import spidev
 import time
 import threading
 
-"""
-ECE dummy functions for testing
-TransmitLock is included to mock concurrency behaviors of ECE functions
-"""
+spi = spidev.SpiDev()
 
 
 class TransmitLock():
@@ -17,6 +16,7 @@ class TransmitLock():
     def __init__(self):
         self.lock = threading.Lock()
         self.is_transmitting = 0
+        self.priority = 0
 
     def can_transmit(self):
         self.lock.acquire()
@@ -29,17 +29,23 @@ class TransmitLock():
         self.is_transmitting = 0
         self.lock.release()
 
-    def start_transmit(self):
+    def start_transmit(self, priority):
         """ Tries to acquire the lock to start transmitting.  If the lock is acquired
         by another thread, sets self.is_transmitting = -1 then returns False.
         """
         self.lock.acquire()
         if self.is_transmitting == 0:
-            self.is_transmitting = 1
-            self.lock.release()
-            return True
+            if self.priority == priority or self.priority == 0:
+                self.is_transmitting = 1
+                self.priority = 0
+                self.lock.release()
+                return True
+            else:
+                self.lock.release()
+                return False
         elif self.is_transmitting == 1:
             self.is_transmitting = -1
+            self.priority = priority
             self.lock.release()
             return False
         else:
@@ -50,103 +56,110 @@ class TransmitLock():
 tlock = TransmitLock()
 
 
+def setSlave(PiBus):
+    """ 
+    set which arduino to talk to. slave(0) for arduino 1 and slave(1) for arduino 2
+    """
+    device = 0
+    bus = PiBus
+    spi.open(device, bus)
+    spi.mode = 0
+    spi.max_speed_hz = 115200
+
+
 def transmit(message):
     try:
         while tlock.can_transmit():
             print(message)
-            # time.sleep([0.1])
+            # tx = spi.writebytes([message])
+            # time.sleep(0.1)
         tlock.end_transmit()
     finally:
-        pass
+        #tx = spi.writebytes([ord('S')])
+        print(message)
+        spi.close()
 
 
 def fwd(power):
-    while not tlock.start_transmit():
+    priority = time.time()
+    while not tlock.start_transmit(priority):
         time.sleep(0.1)
-    cmd = 'Forward'
+    setSlave(1)
+    cmd = ord('F')
+    # print b
+    print(cmd)
     transmit(cmd)
+    # cmd = ord(param) -- do some math on the param to separate different speeds.
+    # Maybe >100 one speed <100 another set speed
 
 
 def back(power):
-    while not tlock.start_transmit():
+    priority = time.time()
+    while not tlock.start_transmit(priority):
         time.sleep(0.1)
-    cmd = 'Back'
+    setSlave(1)
+    cmd = ord('B')
+    # print b
+    print(cmd)
     transmit(cmd)
 
 
 def left(power):
-    while not tlock.start_transmit():
+    priority = time.time()
+    while not tlock.start_transmit(priority):
         time.sleep(0.1)
-    cmd = 'Left'
+    setSlave(1)
+    cmd = ord('L')
+    # print b
+    print(cmd)
     transmit(cmd)
 
 
 def right(power):
-    while not tlock.start_transmit():
+    priority = time.time()
+    while not tlock.start_transmit(priority):
         time.sleep(0.1)
-    cmd = 'Right'
+    setSlave(1)
+    cmd = ord('R')
+    # print b
+    print(cmd)
     transmit(cmd)
 
 
 def stop():
-    while not tlock.start_transmit():
+    priority = time.time()
+    while not tlock.start_transmit(priority):
         time.sleep(0.1)
-    cmd = 'Stop'
+    setSlave(1)
+    cmd = ord('S')
     # print b
     try:
-        i = 0
-        while i < 10:
-            print(cmd)
-            time.sleep(0.1)
-            i += 1
+        print(cmd)
+        for i in range(5):
+            tx = spi.writebytes([cmd])
+        # time.sleep(0.1)
         tlock.end_transmit()
     finally:
-        pass
+        spi.close()
 
 
 def LineFollow():
-    cmd = 'Tape follow'  # "T" = "Tape follow"
+    priority = time.time()
+    while not tlock.start_transmit(priority):
+        time.sleep(0.1)
+    setSlave(1)
+    cmd = ord('T')  # for tape follow
+    # print b
+    print(cmd)
     transmit(cmd)
 
 
 def ObjectDetection():
-    cmd = 'Object detect'
+    priority = time.time()
+    while not tlock.start_transmit(priority):
+        time.sleep(0.1)
+    setSlave(0)
+    cmd = ord('O')
+    # print b
+    print(cmd)
     transmit(cmd)
-
-
-"""
-def fwd(power):
-    print("Moving forward with " + str(power))
-
-
-def back(power):
-    print("Moving backwards with " + str(power))
-
-
-def wait(sec):
-    print("Waiting for " + str(sec) + " seconds")
-
-
-def stop():
-    print("Stopped.")
-
-
-def ECE_wheel_pwr(power):
-    print("Set wheel power to " + str(power))
-
-
-def right(power):
-    print("Turning clockwise with power " + str(power))
-
-
-def left(power):
-    print("Turning counterclockwise with power " + str(power))
-
-
-def ObjectDetection():
-    print("Switching to Object Detection Mode")
-
-
-def LineFollow():
-    print("Switching to Line Follow mode")
-"""
