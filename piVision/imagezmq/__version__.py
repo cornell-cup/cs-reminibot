f4cc90f7e5c51cbd5713c7771c7fd2a87c4eedb7
rachelluuu <luachel18@gmail.com>
2020-02-15 20:57:53 +0000

    re-organize imagezmq files

--- piVision/imagezmq/__version__.py
+++ piVision/imagezmq/__version__.py
@@ -1,9 +1,9 @@
-# populates fields for >>>help(imagezmq)
+# populates fields for >>>help(imagezmq) and setup.py
 __title__ = 'imagezmq'
 __description__ = 'Transporting OpenCV images via ZMQ'
-__url__ = ''
-__version__ = '0.0.2'
+__url__ = 'https://github.com/jeffbass/imagezmq'
+__version__ = '1.0.1'
 __author__ = 'Jeff Bass'
-__author_email__ = 'jeff@yin-yan-ranch.com'
-__license__ = 'MIT 1.0'
-__copyright__ = 'Copyright 2017 Jeff Bass'
+__author_email__ = 'jeff@yin-yang-ranch.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Jeff Bass'
