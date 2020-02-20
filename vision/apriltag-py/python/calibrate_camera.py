35cfcd44133b0bed95803d405b690fe7d93c11ce
Sydney Lawrence <sll224@cornell.edu>
2020-02-20 10:52:08 -0500

    Clean and re-document calibrate_camera.py

--- vision/apriltag-py/python/calibrate_camera.py
+++ vision/apriltag-py/python/calibrate_camera.py
@@ -8,131 +8,13 @@ import time
 import numpy as np
 
 
-def main():
-
-    parser = argparse.ArgumentParser(
-        description='calibrate camera intrinsics using OpenCV')
-
-    parser.add_argument('filenames', metavar='IMAGE', nargs='+',
-                        help='input image files')
-
-    parser.add_argument('-r', '--rows', metavar='N', type=int,
-                        required=True,
-                        help='# of chessboard corners in vertical direction')
-
-    parser.add_argument('-c', '--cols', metavar='N', type=int,
-                        required=True,
-                        help='# of chessboard corners in horizontal direction')
-
-    parser.add_argument('-s', '--size', metavar='NUM', type=float, default=1.0,
-                        help='chessboard square size in user-chosen units (should not affect results)')
-
-    parser.add_argument('-d', '--show-detections', action='store_true',
-                        help='show detections in window')
-
-    options = parser.parse_args()
-
-    if options.rows < options.cols:
-        patternsize = (options.cols, options.rows)
-    else:
-        patternsize = (options.rows, options.cols)
-
-    sz = options.size
-
-    x = np.arange(patternsize[0])*sz
-    y = np.arange(patternsize[1])*sz
-
-    xgrid, ygrid = np.meshgrid(x, y)
-    zgrid = np.zeros_like(xgrid)
-    opoints = np.dstack((xgrid, ygrid, zgrid)).reshape(
-        (-1, 1, 3)).astype(np.float32)
-
-    imagesize = None
-
-    win = 'Calibrate'
-    cv2.namedWindow(win)
-
-    ipoints = []
-
-    for filename in options.filenames:
-
-        rgb = cv2.imread(filename)
-
-        if rgb is None:
-            print('warning: error opening {}, skipping'.format(filename))
-            continue
-
-        cursize = (rgb.shape[1], rgb.shape[0])
-
-        if imagesize is None:
-            imagesize = cursize
-        else:
-            assert imagesize == cursize
-
-        print('loaded ' + filename + ' of size {}x{}'.format(*imagesize))
-
-        if len(rgb.shape) == 3:
-            gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
-        else:
-            gray = rgb
-
-        retval, corners = cv2.findChessboardCorners(gray, patternsize)
-
-        if options.show_detections:
-            display = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
-            cv2.drawChessboardCorners(display, patternsize, corners, retval)
-            cv2.imshow(win, display)
-            while cv2.waitKey(5) not in range(128):
-                pass
-
-        if not retval:
-            print('warning: no chessboard found in {}, skipping'.format(filename))
-        else:
-            ipoints.append(corners)
-
-    flags = (cv2.CALIB_ZERO_TANGENT_DIST |
-             cv2.CALIB_FIX_K1 |
-             cv2.CALIB_FIX_K2 |
-             cv2.CALIB_FIX_K3 |
-             cv2.CALIB_FIX_K4 |
-             cv2.CALIB_FIX_K5 |
-             cv2.CALIB_FIX_K6)
-
-    opoints = [opoints] * len(ipoints)
-
-    retval, K, dcoeffs, rvecs, tvecs = cv2.calibrateCamera(opoints, ipoints, imagesize,
-                                                           cameraMatrix=None,
-                                                           distCoeffs=np.zeros(
-                                                               5),
-                                                           flags=flags)
-
-    assert(np.all(dcoeffs == 0))
-
-    fx = K[0, 0]
-    fy = K[1, 1]
-    cx = K[0, 2]
-    cy = K[1, 2]
-
-    params = (fx, fy, cx, cy)
-
-    print()
-    print('all units below measured in pixels:')
-    print('  fx = {}'.format(K[0, 0]))
-    print('  fy = {}'.format(K[1, 1]))
-    print('  cx = {}'.format(K[0, 2]))
-    print('  cy = {}'.format(K[1, 2]))
-    print()
-    print('pastable into Python:')
-    print('  fx, fy, cx, cy = {}'.format(repr(params)))
-    print()
-
-
 def main_with_video():
-
-    # Parse args
-    # USAGE:
-    # python3 calibrate_camera.py -r <rows> -c <cols>
     """
+    This is the main entry point of the camera calibration script.
+    Run this with the following args:
+
+    python3 calibrate_camera.py -r <rows> -c <cols>
+
     IMPORTANT!
     Make sure the rows entered is (# of boxes in a column) - 1,
     cols = (# of boxes in a row) - 1
@@ -141,6 +23,15 @@ def main_with_video():
     "inner chessboard". This offset will be built in later, but
     that would require changing documentation before an implementation
     has been built.
+
+    Once you've done that, the code will open a program that shows a 
+    picture of the checkerboard and any calibration lines the
+    computer vision library made. Press any key other than
+    ESCAPE to continue the calibration process. It is recommended
+    to press ESCAPE if the camera does not see the entire board.
+
+    The code will then compute what it needs to do to calibrate the camera
+    and write an output in a file typically named 0.calib.
 
     """
 
@@ -198,110 +89,102 @@ def main_with_video():
             point = (i * args['size'], j * args['size'], 0.0)
             checkerboard_points.append(point)
 
-    key = 0
     frame = []
     gray = None
     corners = []
 
     # find the checkerboard
-    # TODO implement key press controls
-    # while key != 27:
-    while True:
-        for i in range(len(cameras)):
-            # Open the camera
-            while not cameras[i].isOpened():
-                cameras[i].open()
-                time.sleep(0.1)
+    # TODO are the key press controls from the old one necessary?
+    for i in range(len(cameras)):
+        # Open the camera
+        while not cameras[i].isOpened():
+            cameras[i].open()
+            time.sleep(0.1)
 
-            # take a snapshot & convert to gray
-            while len(frame) == 0:
-                read_ok, f_ret = cameras[i].read()
-                if read_ok:
-                    frame = f_ret
-                time.sleep(0.1)
-            gray = cv2.cvtColor(frame, COLOR_BGR2GRAY)
+        # take a snapshot & convert to gray
+        while len(frame) == 0:
+            read_ok, f_ret = cameras[i].read()
+            if read_ok:
+                frame = f_ret
+            time.sleep(0.1)
+        gray = cv2.cvtColor(frame, COLOR_BGR2GRAY)
 
-            # Find the checkerboard
-            flags = CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK
-            ret = False
-            # TODO loop this
-            retval, corners = cv2.findChessboardCorners(
-                gray, checkerboard_size, tuple(corners), flags)
-            if len(corners) != 0:
-                print("Found checkerboard on " + str(i))
-                obj_points[i].append(objp)
-                # TODO TERM_CRITERIA_ITER used in C++, using
-                # MAX_ITER OK?
-                win_size = (11, 11)
-                zero_zone = (-1, -1)
-                criteria = (TERM_CRITERIA_EPS +
-                            TERM_CRITERIA_MAX_ITER, 30, 0.1)
-                corners = cornerSubPix(
-                    gray, corners, win_size, zero_zone, criteria)
-                img_points[i].append(corners)
-            assert (frame.any() != None)
-            assert (corners.any() != None)
-            ret = True
-            drawChessboardCorners(frame, checkerboard_size, corners, ret)
-        assert(len(frame) != 0)
-        # TODO re-enable this for demos
-        imshow(str(i), frame)
-        cv2.waitKey(0)
-        break
+        # Find the checkerboard
+        flags = CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + \
+            CALIB_CB_FAST_CHECK
+        ret = False
+        # TODO loop this
+        retval, corners = cv2.findChessboardCorners(
+            gray, checkerboard_size, tuple(corners), flags)
+        if len(corners) != 0:
+            print("Found checkerboard on " + str(i))
+            obj_points[i].append(objp)
+            # TODO TERM_CRITERIA_ITER used in C++, using MAX_ITER OK?
+            win_size = (11, 11)
+            zero_zone = (-1, -1)
+            criteria = (TERM_CRITERIA_EPS +
+                        TERM_CRITERIA_MAX_ITER, 30, 0.1)
+            corners = cornerSubPix(
+                gray, corners, win_size, zero_zone, criteria)
+            img_points[i].append(corners)
+        assert (frame.any() != None)
+        assert (corners.any() != None)
+        ret = True
+        drawChessboardCorners(frame, checkerboard_size, corners, ret)
+    assert(len(frame) != 0)
+    print("Press any key other than ESCAPE to continue")
+    print("Press ESCAPE to abort calibration. If the camera can't see" +
+          "the entire board, you should press ESCAPE and try again.")
+    imshow(str(i), frame)
+    key = cv2.waitKey(0)
+    if key == 27:
+        print("Calibration process aborted.")
+        exit(0)
 
-    print("Image points")
-    print(img_points)
-    print("Object points")
-    print(obj_points)
-    # Write .calib file when 'w' is pressed
-    # while key == 'w':
-    if True:
-        print("Beginning calibration file creation process")
-        camera_matrix = []
-        dist_coeffs = []
-        rvecs = []
-        tvecs = []
-        for i in range(len(cameras)):
-            if not cameras[i].isOpened():
-                continue
-            if len(obj_points[i]) == 0:
-                print("No checkerboards detected on camera" + str(i))
-                continue
+    # Write the calibration file
+    print("Beginning calibration file creation process")
+    camera_matrix = []
+    dist_coeffs = []
+    rvecs = []
+    tvecs = []
+    for i in range(len(cameras)):
+        if not cameras[i].isOpened():
+            continue
+        if len(obj_points[i]) == 0:
+            print("No checkerboards detected on camera" + str(i))
+            continue
 
-            # TODO check if calib exists first
-            print("Calibrating camera " + str(camera_ids[i]))
-            print("Writing 1")
-            print(obj_points[i])
-            obj_points[i] = np.asarray(obj_points[i])
-            print("numpy array")
-            print(obj_points[i])
-            # print(img_points[i])
-            # img_points[i] = np.asarray(img_points[i][0])
-            # TODO debug this line
-            # np.size(frame)
-            # print(obj_points[i].shape)
-            # print(img_points[i].shape)
-            # print(obj_points[i])
-            # print(img_points[i][0])
-            ret_r, mtx_r, dist_r, rvecs_r, tvecs_r = cv2.calibrateCamera(
-                obj_points[i], img_points[i], (len(obj_points), len(obj_points[0])), None, None)
-            # calibrateCamera(
-            # obj_points[i], img_points[i], frame.shape, camera_matrix, dist_coeffs)
-            print("Writing 2")
-            print("Writing calibration file")
-            calib_file = open(str(camera_ids[i]) + ".calib", "w+")
-            calib_file.write("camera_matrix =")
-            write_matrix_to_file(mtx_r, calib_file)
-            calib_file.write("dist_coeffs =")
-            write_matrix_to_file(dist_r, calib_file)
-            print(rvecs_r)
-            print(tvecs_r)
-            calib_file.close()
-            print("Calibration file written to " +
-                  str(camera_ids[i]) + ".calib")
+        # TODO check if calib exists first
+        print("Calibrating camera " + str(camera_ids[i]))
+        obj_points[i] = np.asarray(obj_points[i])
+        ret_r, mtx_r, dist_r, rvecs_r, tvecs_r = cv2.calibrateCamera(
+            obj_points[i], img_points[i],
+            (len(obj_points), len(obj_points[0])), None, None)
+        print("Writing calibration file")
+        calib_file = open(str(camera_ids[i]) + ".calib", "w+")
+        calib_file.write("camera_matrix =")
+        write_matrix_to_file(mtx_r, calib_file)
+        calib_file.write("dist_coeffs =")
+        write_matrix_to_file(dist_r, calib_file)
+        calib_file.close()
+        print("Calibration file written to " +
+              str(camera_ids[i]) + ".calib")
 
 
 def write_matrix_to_file(matrix, file):
+    """
+    Writes a matrix to a file.
+
+    Args:
+      matrix (numpy.ndarray): The numpy matrix to write.
+
+      file (_io.TextIOWrapper): The file to write to, typically
+      found with open(). The file must be open with write permissions
+      BEFORE it is passed in to this function.
+
+    Returns: Nothing.
+    """
+
     num_rows, num_cols = matrix.shape
     for r in range(num_rows):
         for c in range(num_cols):
