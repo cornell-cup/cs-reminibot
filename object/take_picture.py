0874a5e2f20136b1f068ba549e4b9e86ff865a09
Gergely Chikan <gc392@cornell.edu>
2020-04-25 14:06:33 -0500

    Implemented argparse in take_picture.py

--- object/take_picture.py
+++ object/take_picture.py
@@ -1,79 +1,82 @@
 import cv2
 import sys
 import os
+import argparse
+from random import random
 
+def take_pictures(args):
+    image_counter = 0
 
-args = sys.argv
-color = True
-image_counter = 0
-image_name = 'img'
-path = os.getcwd() + '/images'
-image_dim = (600, 600)
-window_dim = (600, 600)
+    # Opens the camera
+    cv2.namedWindow("Capture")
+    cap = cv2.VideoCapture(0)
 
-i = 1
-while i + 1 < len(args):
-    try:
-        if args[i] == '-dim':
-            try:
-                if int(args[i+1]) < 1 or int(args[i+2]) < 1:
-                    raise ValueError
-                image_dim = (int(args[i+1]),int(args[i+2]))
-                i += 2
-            except ValueError:
-                print('Expected two positive integers after \'-dim\' ')
-        elif args[i] == '-window':
-            try:
-                if int(args[i+1]) < 100 or int(args[i+2]) < 100:
-                    raise ValueError
-                window_dim = (int(args[i+1]),int(args[i+2]))
-                i += 2
-            except ValueError:
-                print('Expected two integers over 100 after \'-window\' ')
-        elif args[i] == '-folder':
-            path = os.getcwd() + args[i+1]
-            i += 1
-        elif args[i] == '-name':
-            image_name = args[i+1]
-            i += 1
-        else:
-            print('Unexpected argument \'' + args[i] + '\'')
-        i += 1
-    except IndexError:
-        print('Expected more arguments')
+    if not cap.isOpened():
+        cap = cv2.VideoCapture(-1)
 
-cv2.namedWindow("Capture")
-cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Not opening")
+        exit(0)
 
-if not cap.isOpened():
-    cap = cv2.VideoCapture(-1)
+    while cap.isOpened():
 
-if not cap.isOpened():
-    print("Not opening")
-    exit(0)
+        # Gets webcam feed
+        ret, frame = cap.read()
+        key = cv2.waitKey(1)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    key = cv2.waitKey(1)
+        # Formats output video feed
+        if not args.color:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.resize(frame, args.image_dim)
+        frame = cv2.flip(frame, 1)
+        shown_frame = cv2.resize(frame, args.window_dim)
 
-    if not color:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.resize(frame, image_dim)
-    frame = cv2.flip(frame, 1)
-    shown_frame = cv2.resize(frame, window_dim)
+        cv2.imshow('Capture', shown_frame)
 
-    cv2.imshow('Capture', shown_frame)
+        #Takes a picture on space
+        if key & 0xFF == ord(' '):
+            # Determines whether image is for training or validation
+            rand = random()
+            if rand < args.ratio:
+                type = "training"
+            else:
+                type = "validation"
+            path = os.getcwd() + '/' + args.folder + '/' + type + '/' + args.category + '/'
+            # Makes directorties for given path
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            # Writes the capture to file
+            cv2.imwrite(path + '/' + args.image_name + '_' + str(image_counter) + '.jpg', frame)
+            image_counter += 1
+        # Quits on escape or 'q'
+        if key & 0xFF in [27, 1048603, ord('q')]:
+            break
 
+    cap.release()
+    cv2.destroyAllWindows()
 
-    if key & 0xFF == ord('c'):
-        color = not color
-    if key & 0xFF == ord(' '):
-        if not os.path.isdir(path):
-            os.makedirs(path)
-        cv2.imwrite(path + '/' + image_name + '_' + str(image_counter) + '.jpg', frame)
-        image_counter = image_counter + 1
-    if key & 0xFF in [27, 1048603, ord('q')]:
-        break
+if __name__=="__main__":
+    # Defines arguments for parser
+    parser = argparse.ArgumentParser(description='Take in arguments for taking pictures')
+    parser.add_argument("-color", "-col", action="store",type=bool,dest="color", default="True")
+    parser.add_argument("-name", "-n", action="store", dest="image_name", default="img")
+    parser.add_argument("-dim", "-d", nargs='*', action="store", dest="image_dim", type=int, default=[600,600])
+    parser.add_argument("-window", "-w", nargs='*', action="store", dest="window_dim", type=int, default=[600,600])
+    parser.add_argument("-folder", "-f", action="store", dest="folder", default="images")
+    parser.add_argument("-category", "-c", action="store", dest="category", default="True")
+    parser.add_argument("-ratio", "-r", action="store", dest="ratio", default=.2, type=float)
+    args = parser.parse_args()
 
-cap.release()
-cv2.destroyAllWindows()
+    # Additional constrainst on arguments
+    if args.image_dim is not None and len(args.image_dim) != 2:
+        parser.error('-dim expected two values')
+    if args.window_dim is not None and len(args.window_dim) != 2:
+        parser.error('-window expected two values')
+    if args.ratio < 0 or args.ratio > 1:
+        parser.error('x must be in [0,1] in \'-ratio x\'')
+
+    # Converts image_dim and window_dim from array to tuple
+    args.image_dim = (args.image_dim[0],args.image_dim[1])
+    args.window_dim = (args.window_dim[0],args.window_dim[1])
+
+    take_pictures(args)
