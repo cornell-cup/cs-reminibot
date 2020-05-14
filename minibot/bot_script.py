3ee629516f434eb34fb49ef801a3abe84539ea31
Viren Shah <vvs24@cornell.edu>
2020-05-13 22:57:23 -0400

    Trying to move servo with ultrasonic

--- minibot/bot_script.py
+++ minibot/bot_script.py
@@ -3,14 +3,12 @@ import time
 from threading import *
 def run():
     for count in range(1):
-      # time.sleep(5)
-      # Thread(target=stop, args=[]).start()
+      time.sleep(3)
+      Thread(target=stop, args=[]).start()
       if 0 < read_ultrasonic() <= 10:
-        # move_servo(90)
-        pass
+        move_servo(90)
       else:
-        # move_servo(0)
-        pass
+        move_servo(0)
 
 run()
     
