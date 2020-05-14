b4aadd5ed567be5febab17b6b90682702e6d3807
Viren Shah <vvs24@cornell.edu>
2020-05-13 22:59:56 -0400

    Trying 5 iterations

--- minibot/bot_script.py
+++ minibot/bot_script.py
@@ -2,7 +2,7 @@ from scripts.PiArduino import *
 import time
 from threading import *
 def run():
-    for count in range(1):
+    for count in range(5):
       time.sleep(3)
       Thread(target=stop, args=[]).start()
       if 0 < read_ultrasonic() <= 10:
