e12df933094ed520c281365c11a579e9d56d3342
Sydney Lawrence <sll224@cornell.edu>
2020-10-24 15:46:13 -0400

    Add board distortion view

--- vision/apriltag-py/python/test_calib.py
+++ vision/apriltag-py/python/test_calib.py
@@ -1,6 +1,6 @@
 import cv2
 import util
-
+import numpy as np
 """
 This file qualitatively shows what happens when a
 checkerboard calibration is performed.
@@ -16,12 +16,41 @@ Instructions for use:
 
 
 def main():
+    # Get checkerboard
     cols, rows = 9, 6
     camera = get_camera()
-    corners = get_checkerboard_interactive(camera,cols, rows)
+    image, gray_image, corners = get_checkerboard_interactive(camera,cols, rows)
     print(corners)
     cv2.waitKey(0)
     camera.release()
+
+    # Compute transform matrix
+    # From tutorial:
+    # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html#calibration
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    obj_points = np.zeros((6*9,3), np.float32)
+    obj_points[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+    img_points = cv2.cornerSubPix(gray_image, corners, (11,11), (-1,-1), criteria)
+    ret, mat, dist, rvecs, tvecs = cv2.calibrateCamera(
+        [obj_points],
+        [img_points],
+        gray_image.shape[::-1],
+        None,
+        None
+    )
+    height, width = image.shape[:2]
+    new_mtx, roi = cv2.getOptimalNewCameraMatrix(mat, dist, (width,height), 1, (width,height))
+
+    # Undistort
+    dst = cv2.undistort(image, mat, dist, None, new_mtx)
+
+    # Crop
+    x,y,w,h = roi
+    dst = dst[y:y+height, x:x+width]
+
+    # Show new image
+    cv2.imshow("Converted image", dst)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 def get_checkerboard_interactive(camera, cols, rows):
@@ -39,7 +68,7 @@ def get_checkerboard_interactive(camera, cols, rows):
         cv2.imshow("Checkerboard Calibration", image)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-    return corners
+    return image, gray_image, corners
 
 def get_image_on_keypress(camera):
     image = None
