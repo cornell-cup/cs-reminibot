cf27009deefe3547c977f96ef1a8f046e2a48db2
Gergely Chikan <gc392@cornell.edu>
2020-11-17 12:44:35 -0600

    Added Object Classification Tab

---
+++ static/gui/static/js/components/Object.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class ObjectClassification extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h1>HELLO</h1>
+    )
+  }
+}
