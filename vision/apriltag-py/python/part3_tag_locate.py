60f5dfecbf3bff071d82b4603a8bdd5ee0dc3b22
Sydney Lawrence <sll224@cornell.edu>
2020-11-30 19:14:38 -0500

    Error visualization and transform WIP

--- vision/apriltag-py/python/part3_tag_locate.py
+++ vision/apriltag-py/python/part3_tag_locate.py
@@ -1,12 +1,11 @@
-from cv2 import *
+import cv2
 import apriltag
 import argparse
 import numpy as np
 import sys
 import time
 import requests
-from util import get_image, get_matrices_from_file, undistort_image, get_offsets_from_file
-from util import compute_tag_undistorted_pose, get_numpy_matrix, read_calib_json
+import util
 
 # Constants
 DEVICE_ID = 0  # The device the camera is, usually 0. TODO make this adjustable
@@ -24,27 +23,20 @@ def main():
     num_frames = 0  # number of frames processed
 
     args = get_args()
-    url = args['url']
-    SEND_DATA = (args['url'] != None)
-    calib_file_name = args['file']
-    TAG_SIZE = args['size']
+    url = args["url"]
+    SEND_DATA = args["url"] != None
+    calib_file_name = args["file"]
+    TAG_SIZE = args["size"]
     calib_file = open(calib_file_name)
 
-    camera = VideoCapture(DEVICE_ID)  # Open the camera & set camera arams
-    if (not VideoCapture.isOpened(camera)):
-        print("Failed to open video capture device")
-        exit(0)
-    camera.set(CAP_PROP_FRAME_WIDTH, 1280)
-    camera.set(CAP_PROP_FRAME_HEIGHT, 720)
-    camera.set(CAP_PROP_FPS, 30)
+    camera = util.get_camera(DEVICE_ID)
 
     # Get matrices from calibration file
     print("Parsing calibration file " + calib_file_name + "...")
-    calib_file, calib_data = read_calib_json(calib_file_name)
-    transform_matrix = get_numpy_matrix(
-        calib_data, "transform_matrix")
-    camera_matrix = get_numpy_matrix(calib_data, "camera_matrix")
-    dist_coeffs = get_numpy_matrix(calib_data, "dist_coeffs")
+    calib_file, calib_data = util.read_calib_json(calib_file_name)
+    transform_matrix = util.get_numpy_matrix(calib_data, "transform_matrix")
+    camera_matrix = util.get_numpy_matrix(calib_data, "camera_matrix")
+    dist_coeffs = util.get_numpy_matrix(calib_data, "dist_coeffs")
     x_offset = calib_data["offsets"]["x"]
     y_offset = calib_data["offsets"]["y"]
     calib_file.close()
@@ -55,9 +47,9 @@ def main():
     # x_offset, y_offset = get_offsets_from_file(calib_file)
     # calib_file.close()
 
-    assert (camera_matrix.shape == (3, 3))
-    assert (dist_coeffs.shape == (1, 5))
-    assert (transform_matrix.shape == (4, 4))
+    assert camera_matrix.shape == (3, 3)
+    assert dist_coeffs.shape == (1, 5)
+    assert transform_matrix.shape == (4, 4)
 
     # print("TRANSFORM MATRIX:\n {}\n".format(transform_matrix))
     print("Calibration file parsed successfully.")
@@ -85,8 +77,8 @@ def main():
             exit(0)
 
         # take a picture and get detections
-        frame = get_image(camera)
-        gray = cvtColor(frame, COLOR_BGR2GRAY)
+        frame = util.get_image(camera)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Un-distorting an image worsened distortion effects
         # Uncomment this if needed
@@ -102,8 +94,9 @@ def main():
             # TODO draw tag - might be better to generalize, because
             # locate_cameras does this too.
 
-            (x, y, z, angle) = compute_tag_undistorted_pose(
-                camera_matrix, dist_coeffs, transform_matrix, d, TAG_SIZE)
+            (x, y, z, angle) = util.compute_tag_undistorted_pose(
+                camera_matrix, dist_coeffs, transform_matrix, d, TAG_SIZE
+            )
 
             # Scale the coordinates, and print for debugging
             # prints Device ID :: tag id :: x y z angle
@@ -111,28 +104,28 @@ def main():
             x = MULT_FACTOR * (x + x_offset)
             y = MULT_FACTOR * (y + y_offset)
             # print(tag_xyz)
-            print("{} :: {} :: {} {} {} {}".format(
-                DEVICE_ID, d.tag_id, x, y, z, angle))
+            print("{} :: {} :: {} {} {} {}".format(DEVICE_ID, d.tag_id, x, y, z, angle))
 
             # Send the data to the URL specified.
             # This is usually a URL to the base station.
             if SEND_DATA:
-                payload = {
-                    "id": d.tag_id,
-                    "x": x,
-                    "y": y,
-                    "orientation": angle
-                }
+                payload = {"id": d.tag_id, "x": x, "y": y, "orientation": angle}
                 r = requests.post(url, json=payload)
                 status_code = r.status_code
                 if status_code / 100 != 2:
                     # Status codes not starting in '2' are usually error codes.
-                    print("WARNING: Basestation returned status code {}".format(
-                        status_code))
+                    print(
+                        "WARNING: Basestation returned status code {}".format(
+                            status_code
+                        )
+                    )
                 else:
                     num_frames += 1
-                    print("Vision FPS (Vision System outflow): {}".format(
-                        num_frames / (time.time() - past_time)))
+                    print(
+                        "Vision FPS (Vision System outflow): {}".format(
+                            num_frames / (time.time() - past_time)
+                        )
+                    )
     pass
 
 
@@ -149,8 +142,9 @@ def get_transform_matrix(file):
     # TODO generalize this? Code is nearly duplicated
     # in locate_cameras.py.
     temp_line = file.readline()  # reconstruct camera matrix
-    transform_matrix_items = list(map(lambda x: float(x),
-                                      temp_line[len("transform_matrix = "):].split(" ")))
+    transform_matrix_items = list(
+        map(lambda x: float(x), temp_line[len("transform_matrix = ") :].split(" "))
+    )
     transform_matrix = np.reshape(np.asarray(transform_matrix_items), (4, 4))
     return transform_matrix
 
@@ -159,21 +153,35 @@ def get_args():
     """
     Get the arguments that were passed in.
     """
-    parser = argparse.ArgumentParser(
-        description='Locate and send Apriltag poses')
+    parser = argparse.ArgumentParser(description="Locate and send Apriltag poses")
 
-    parser.add_argument('-u', '--url',
-                        metavar='<url>',
-                        type=str, required=False,
-                        help='URL to send data to')
+    parser.add_argument(
+        "-u",
+        "--url",
+        metavar="<url>",
+        type=str,
+        required=False,
+        help="URL to send data to",
+    )
 
-    parser.add_argument('-f', '--file', metavar='<calib file name>',
-                        type=str, required=True,
-                        help='.calib file to use for un-distortion')
+    parser.add_argument(
+        "-f",
+        "--file",
+        metavar="<calib file name>",
+        type=str,
+        required=True,
+        help=".calib file to use for un-distortion",
+    )
 
-    parser.add_argument('-s', '--size', metavar='<size>',
-                        type=float, required=False, default=6.5,
-                        help='size of tags to detect')
+    parser.add_argument(
+        "-s",
+        "--size",
+        metavar="<size>",
+        type=float,
+        required=False,
+        default=6.5,
+        help="size of tags to detect",
+    )
 
     options = parser.parse_args()
     args = vars(options)  # get dict of args parsed
@@ -181,5 +189,5 @@ def get_args():
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
