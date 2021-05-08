8932d26c6ac7d149a4efed0d811941106e04e279
Hantian Zhang <hz467@cornell.edu>
2021-05-08 13:37:03 -0400

    resize displayed frame

--- basestation/piVision/server.py
+++ basestation/piVision/server.py
@@ -83,88 +83,89 @@ while True:
 
     # resize the frame to have a maximum width of 400 pixels, then
     # grab the frame dimensions and construct a blob
-    frame = imutils.resize(frame, width=400, inter=cv2.INTER_NEAREST)
+    frame = imutils.resize(frame, width=700, inter=cv2.INTER_NEAREST)
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-                                 0.007843, (300, 300), 127.5)
+    # blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+    #                              0.007843, (300, 300), 127.5)
 
     # pass the blob through the network and obtain the detections and
     # predictions
-    net.setInput(blob)
-    detections = net.forward()
+    # net.setInput(blob)
+    # detections = net.forward()
 
     # reset the object count for each object in the CONSIDER set
-    objCount = {obj: 0 for obj in CONSIDER}
+    #objCount = {obj: 0 for obj in CONSIDER}
 
     # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
+    # for i in np.arange(0, detections.shape[2]):
+    #     # extract the confidence (i.e., probability) associated with
+    #     # the prediction
+    #     confidence = detections[0, 0, i, 2]
 
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # detections
-            idx = int(detections[0, 0, i, 1])
+    #     # filter out weak detections by ensuring the confidence is
+    #     # greater than the minimum confidence
+    #     if confidence > args["confidence"]:
+    #         # extract the index of the class label from the
+    #         # detections
+    #         idx = int(detections[0, 0, i, 1])
 
-            # check to see if the predicted class is in the set of
-            # classes that need to be considered
-            if CLASSES[idx] in CONSIDER:
-                # increment the count of the particular object
-                # detected in the frame
-                objCount[CLASSES[idx]] += 1
+    #         # check to see if the predicted class is in the set of
+    #         # classes that need to be considered
+    #         if CLASSES[idx] in CONSIDER:
+    #             # increment the count of the particular object
+    #             # detected in the frame
+    #             objCount[CLASSES[idx]] += 1
 
-                # compute the (x, y)-coordinates of the bounding box
-                # for the object
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
+    #             # compute the (x, y)-coordinates of the bounding box
+    #             # for the object
+    #             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    #             (startX, startY, endX, endY) = box.astype("int")
 
-                # draw the bounding box around the detected object on
-                # the frame
-                cv2.rectangle(frame, (startX, startY), (endX, endY),
-                              (255, 0, 0), 2)
+    #             # draw the bounding box around the detected object on
+    #             # the frame
+    #             cv2.rectangle(frame, (startX, startY), (endX, endY),
+    #                           (255, 0, 0), 2)
 
     # draw the sending device name on the frame
     cv2.putText(frame, rpiName, (10, 25),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # draw the object count on the frame
-    label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
-                      objCount.items())
-    cv2.putText(frame, label, (10, h - 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
+    #                   objCount.items())
+    # cv2.putText(frame, label, (10, h - 20),
+    #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # update the new frame in the frame dictionary
-    frameDict[rpiName] = frame
+    # frameDict[rpiName] = frame
 
     # build a montage using images in the frame dictionary
-    montages = build_montages(frameDict.values(), (w, h), (mW, mH))
+    # montages = build_montages(frameDict.values(), (w, h), (mW, mH))
 
     # display the montage(s) on the screen
-    for (i, montage) in enumerate(montages):
-        cv2.imshow("On-Bot Video Stream ({})".format(i),
-                   montage)
-        #print(i, datetime.now())
+    # for (i, montage) in enumerate(montages):
+    #     cv2.imshow("On-Bot Video Stream ({})".format(i),
+    #                montage)
+    #print(i, datetime.now())
+    cv2.imshow("On-Bot Video Stream", frame)
 
     # detect any kepresses
     key = cv2.waitKey(1) & 0xFF
 
     # if current time *minus* last time when the active device check
     # was made is greater than the threshold set then do a check
-    if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-        # loop over all previously active devices
-        for (rpiName, ts) in list(lastActive.items()):
-            # remove the RPi from the last active and frame
-            # dictionaries if the device hasn't been active recently
-            if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-                print("[INFO] lost connection to {}".format(rpiName))
-                lastActive.pop(rpiName)
-                frameDict.pop(rpiName)
+    # if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
+    #     # loop over all previously active devices
+    #     for (rpiName, ts) in list(lastActive.items()):
+    #         # remove the RPi from the last active and frame
+    #         # dictionaries if the device hasn't been active recently
+    #         if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
+    #             print("[INFO] lost connection to {}".format(rpiName))
+    #             lastActive.pop(rpiName)
+    #             frameDict.pop(rpiName)
 
-        # set the last active check time as current time
-        lastActiveCheck = datetime.now()
+    #     # set the last active check time as current time
+    #     lastActiveCheck = datetime.now()
 
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
