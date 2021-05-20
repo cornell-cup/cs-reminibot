c4357c35b7047fc073cd2662a7a19f4a69c83b88
slawrence100 <sll224@cornell.edu>
2021-05-20 16:01:18 +0100

    Add photoresistor example

--- minibot/scripts/spi_read_crc_example.py
+++ minibot/scripts/spi_read_crc_example.py
@@ -1,6 +1,5 @@
 """
-Imagine you need to receive a complex set of inputs
-from the Arduino.
+Imagine you need to receive sensor data from the Arduino.
 
 Here's an example of how you might do that...
 """
@@ -19,8 +18,10 @@ spi.mode = 0
 spi.max_speed_hz = 115200
 
 # To load something, build a request for it
-# This assumes the arduino knows to load item 1 (which is arbitrary)
-load_req = [ord('L'), ord('O'), ord('A'), ord('D'), 10]
+# This assumes the arduino knows to load item 4
+# This is arbitrary, but I set 4 to correspond to
+# a photoresistor wired to the Arduino itself.
+load_req = [ord('L'), ord('O'), ord('A'), ord('D'), 4]
 load_msg = make_crc_message(load_req)
 
 # Then, make the request for 22 bytes
@@ -30,5 +31,11 @@ data = read_data(spi, load_msg, 22, validate_crc_message)
 # Then remove the start, end, and checksum to get your data
 data = unpack_crc_message(data)
 print(f"Your data: {data}")
+
 # What you do with that data afterwards is up to you...
-# It's usually a good idea to send it to the base station.
+# I know the photoresistor returns an integer, so I parse it
+# and print it.
+sensor_val = int.from_bytes(bytes(data[0:4]), 'big', signed=True)
+print(f"Sensor reads: {sensor_val}")
+
+# It's usually a good idea to send processed data to the base station.
