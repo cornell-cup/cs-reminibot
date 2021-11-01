3c8467de7b502db98aacf5e4c08109b59c988059
Trenton Lyke <tjl223@cornell.edu>
2021-11-01 15:32:22 -0400

    implemented side nav

--- static/gui/static/js/components/Navbar.js
+++ static/gui/static/js/components/Navbar.js
@@ -11,6 +11,7 @@ import RegisterModal from './Login/RegisterModal.js';
 const Navbar = (props) => {
   const [loginEmail, setLoginEmail] = useState(props.cookies.get('current_user_email') || "");
   const [isLoggedIn, setIsLoggedIn] = useState(props.cookies.get(isLoggedIn !== ""));
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
 
   useEffect(() => {
     const current_user_email = props.cookies.get('current_user_email') || "";
@@ -27,37 +28,57 @@ const Navbar = (props) => {
     setIsLoggedIn(false);
     setLoginEmail("");
   }
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    setIsSideNavOpen(true);
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    setIsSideNavOpen(false);
+  }
+
+  function toggleNav() {
+    if(isSideNavOpen){
+      closeNav();
+    }
+    else{
+      openNav();
+    }
+  }
+
   return (
+<div>
+<div id="mySidenav" class="sidenav">
+  <a href="#" class="closebtn" onClick={closeNav}>&times;</a>
+  {isLoggedIn && <a className="nav-link"><FontAwesomeIcon icon={Icons.faUser} /> &nbsp; {loginEmail.substring(0, loginEmail.indexOf('@'))}</a> }
+  <a id="setup-control-link" data-toggle="tab" href="#setup_control_tab" className="nav-link"><FontAwesomeIcon icon="cogs" /> Setup/Movement</a>
+  <a id="coding-link" data-toggle="tab" href="#coding-tab" className="nav-link"><FontAwesomeIcon icon="code" /> Coding</a>
+  {isLoggedIn &&
+              /**TODO: fix the icon for analytics to be something more suited for analytics*/
+              <a id="analytics-link" data-toggle="tab" href="#analytics-tab" className="nav-link"><FontAwesomeIcon icon="cogs" /> Analytics</a>
+            }
+  {isLoggedIn ? <a className="nav-link" onClick={handleLogout}>Logout</a> : <a className="nav-link" data-toggle="modal" data-target="#loginModal">Login</a>}
+  {!isLoggedIn && <a className="nav-link" data-toggle="modal" data-target="#registerModal">Signup</a>}
+</div>
 
     <div id="top-nav" className="mb-4">
-      <nav className="container navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
           <a className="navbar-brand" href="#">
             <img src="./static/img/logo.png" width="50" height="50" className="d-inline-block align-top" alt="" />
             Minibot
           </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="" type="button" onClick={toggleNav} data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item" ><a id="setup-control-link" data-toggle="tab" href="#setup_control_tab" className="nav-link"><FontAwesomeIcon icon="cogs" /> Setup/Movement</a></li>
-            <li className="nav-item" ><a id="coding-link" data-toggle="tab" href="#coding-tab" className="nav-link"><FontAwesomeIcon icon="code" /> Coding</a></li>
-            {isLoggedIn &&
-              /**TODO: fix the icon for analytics to be something more suited for analytics*/
-              <li className="nav-item"><a id="analytics-link" data-toggle="tab" href="#analytics-tab" className="nav-link"><FontAwesomeIcon icon="cogs" /> Analytics</a></li>
-            }
-
-            <li className="nav-item" >{isLoggedIn ? <a className="nav-link" onClick={handleLogout}>Logout</a> : <a className="nav-link" data-toggle="modal" data-target="#loginModal">Login</a>}</li>
-            <li className="nav-item" >{isLoggedIn ? <a className="nav-link"><FontAwesomeIcon icon={Icons.faUser} /></a> : <button className="nav-link" data-toggle="modal" data-target="#registerModal">Signup</button>}</li>
-            {/* {!this.state.isLoggedIn ? <button type="button" data-toggle="modal" data-target="#loginModal">Login</button> : null}
-            {!this.state.isLoggedIn ? <button type="button" data-toggle="modal" data-target="#registerModal">Signup</button> : null} */}
-            {/* {this.state.isLoggedIn ? <label className="white-label"> Logged in as: {this.state.loginEmail} &nbsp; </label> : null}
-            {this.state.isLoggedIn ? <Button id="logout" name="Logout" onClick={this.logout}/> : null} */}
-</ul>
-        </div>
+  </div>
       </nav>
       <LoginModal />
       <RegisterModal />
+    </div>
     </div>
   )
 }
