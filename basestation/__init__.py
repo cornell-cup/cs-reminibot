461c38d0cd45cc51be497a392f4dac5678a241c2
swathi-parthibha <swathiparthibha@gmail.com>
2022-01-30 22:41:25 -0500

    analytics

--- basestation/__init__.py
+++ basestation/__init__.py
@@ -11,8 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_filename}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
-from basestation.user_database import Program, User
-
+from basestation.user_database import Submission, User
 db.init_app(app)
 with app.app_context():
     db.create_all()
--- basestation/base_station.py
+++ basestation/base_station.py
@@ -3,7 +3,7 @@ Base Station for the MiniBot.
 """
 
 from basestation.bot import Bot
-from basestation.user_database import Program, User
+from basestation.user_database import Submission, User
 from basestation import db
 from basestation.util.stoppable_thread import StoppableThread, ThreadSafeVariable
 
@@ -76,7 +76,7 @@ class BaseStation:
         # empty string means 0.0.0.0, which is all IP addresses on the local
         # machine, because some machines can have multiple Network Interface
         # Cards, and therefore will have multiple ip_addresses
-        server_address = ("0.0.0.0", 9434)
+        server_address = ("0.0.0.0", 5001)
 
         # only bind in debug mode if you are the debug server, if you are the
         # monitoring program which restarts the debug server, do not bind,
@@ -225,7 +225,10 @@ class BaseStation:
         for line in program_lines:
             match = regex.match(line)
             if match:
-                func = self.blockly_function_map[match.group(2)]
+                if match.group(2) in self.blockly_function_map:
+                    func = self.blockly_function_map[match.group(2)]
+                else:
+                    func = match.group(2)
                 args = match.group(3)
                 whitespace = match.group(1)
                 if not whitespace:
@@ -238,7 +241,6 @@ class BaseStation:
                 parsed_program.append(parsed_line)
             else:
                 parsed_program.append(line + '\n')  # "normal" Python
-
         parsed_program_string = "".join(parsed_program)
 
         # Now actually send to the bot
@@ -407,3 +409,34 @@ class BaseStation:
     def login_email(self, email: str):
         """Sets the login email property"""
         self._login_email = email
+
+    # data analytics
+
+    def get_user(self, email: str) -> User:
+        user = User.query.filter_by(email=email).first()
+        return user
+
+    def save_submission(self, code: str, email: str) -> Submission:
+        submission = Submission(
+            code=code,
+            time=time.strftime("%Y/%b/%d %H:%M:%S", time.localtime()),
+            duration=-1,
+            user_id=self.get_user(email).id
+        )
+        db.session.add(submission)
+        db.session.commit()
+        return submission
+
+    def update_result(self, result: str, submission_id: int):
+        if submission_id is None:
+            return
+        print("UPDATE RESULT!!!")
+        print(result)
+        submission = Submission.query.filter_by(id=submission_id).first()
+        submission.result = result
+        db.session.commit()
+
+    def get_all_submissions(self, user: User) -> []:
+        submissions = []
+        submissions = Submission.query.filter_by(user_id=User.id)
+        return submissions
