ddec52111126fd912f7c1012350fb75c3ed77bba
Trenton Lyke <tjl223@cornell.edu>
2022-02-12 14:46:50 -0500

    removed some bugs in merge

--- basestation/base_station.py
+++ basestation/base_station.py
@@ -77,22 +77,13 @@ class BaseStation:
         # machine, because some machines can have multiple Network Interface
         # Cards, and therefore will have multiple ip_addresses
         server_address = ("0.0.0.0", 5001)
-        # Send a message on a specific port so that the minibots can discover the ip address
-        # of the computer that the BaseStation is running on.
-        self.broadcast_ip_thread = threading.Thread(
-            target=self.broadcast_ip, daemon=True
-        )
-
-        self.bot_discover_thread = threading.Thread(
-            target=self.discover_and_create_bots, daemon=True
-        )
+        
 
         self.vision_monitior_thread = threading.Thread(
             target=self.vision_monitior, daemon=True
         )
 
-        self.broadcast_ip_thread.start()
-        self.bot_discover_thread.start()
+        
         # checks if vision can see april tag by checking lenth of vision_log
         self.vision_monitior_thread.start()
         # self.connections = BaseConnection()
