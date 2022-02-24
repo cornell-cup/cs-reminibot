5712610eea9381f8b04e90b0a3f7020d8d0456a4
Sanjana679 <sanjana@nandi.net>
2022-02-24 17:55:14 -0500

    Updated ir code, attemping with spi

--- minibot/scripts/arduino code/ir_test.ino
+++ minibot/scripts/arduino code/ir_test.ino
@@ -1,33 +1,114 @@
+// Bottom Arduino Motors, US sensor, reflectance, IR
+// Top Arduino Speaker, RFID
+#include <Adafruit_PN532.h>
+#include <QTRSensors.h>
 #include <SPI.h>
-#include <Servo.h>
+#include <Wire.h>
+QTRSensors qtr;
+
+// Bi-directinal SPI
+// 13 SCK  (serial clock)
+// 12 MISO (master in slave out)
+// 11 MOSI (master out slave in)
+// 10 SS (slave select)
+
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+// RFID module
+#define PN532_IRQ (2)   // pin 3 of the RJ12 17 (2)
+#define PN532_RESET (3) // pin 4 of the RJ12 9  (3)
+
+// IR
+int IRPin = 4;
+
+int send_data[6];
+
+// Or use this line for a breakout or shield with an I2C connection:
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
 // initialize the buffer
 int bufSize = 4;
 char buf[4];
+volatile byte pos = 0;
 
-int IRreciever = 6;
+boolean valid;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    SPCR |= bit(SPE);  // turn on SPI in slave mode
-
-    // set up pin
-    pinMode(IRreciever, INPUT);
-
+    pinMode(SCK, INPUT);
+    pinMode(MISO, OUTPUT);
+    pinMode(MOSI, INPUT);
+    SPCR |= bit(SPE); // turn on SPI in slave mode
+    // turn on the interrupt
     SPI.attachInterrupt();
+
+    pos = 0;
+    pinMode(IRPin, INPUT);
 }
 
-// SPI interrupt routine
-ISR(SPI_STC_vect) {
-    byte c = SPDR;
-    SPDR = c + 10;
-}  // end of interrupt service routine (ISR) for SPI
+// SPI ISR (Interrupt Service Routine)
 
-void loop() {
-    // put your main code here, to run repeatedly;
+ISR(SPI_STC_vect)
+{
+    byte c = SPDR; // get byte from the SPI data register
+    // detect the beginning of the buffer, do not put it in the buffer
+    if (c == '\n')
+    {
+        valid = true;
+    }
+    // detect the end character
+    else if (c == '\r')
+    {
+        valid = false;
+        pos = 0;
+    }
+    // put data into the buffer
+    if ((valid == true) && (c != '\n') && (c != '\r'))
+    {
+        if (pos < bufSize)
+        { /// sizeof buffer
+            buf[pos] = c;
+            pos++;
+        }
+    }
+}
+
+int val;
+void read_IR()
+{
+    val = digitalRead(IRPin);
+    SPDR = val;
+    Serial.println(val);
+}
+
+boolean read_ir = false;
+void check_buffer()
+{
+    if (buf[0] == 'T')
+    {
+        read_ir = true;
+    }
+    else
+    {
+        read_ir = false;
+    }
+}
+
+void loop()
+{
+
     delay(100);
+    check_buffer();
+    if (read_ir == true)
+    {
+        read_IR();
+    }
 
-    int statusSensor = digitalRead(IRreciever);
-    SPDR = statusSensor;
-    Serial.println(statusSensor);
+    // clear the buffer when a command is executed
+    if (valid)
+    {
+        pos = 0;
+        valid = false;
+    }
 }
