d8e39a31bdc3a4cb29ea146b2f4d5ef2f872c7d0
Trenton-Lyke <trentonlyke@gmail.com>
2022-03-04 03:30:46 -0500

    attempting hyprid apriltag library

--- vision/apriltag-py/python/detector.py
+++ vision/apriltag-py/python/detector.py
@@ -1,5 +1,13 @@
 import cv2
-import pyAprilTag
+import sys
+try:
+  import pyAprilTag
+except ImportError:
+  print("could not import pyAprilTag")
+  try:
+    import apriltag
+  except:
+    print("Unable to import any apriltag detection libraries please check the installation of the vision system")
 import util
 from detection import Detection
 
@@ -7,19 +15,24 @@ from detection import Detection
 
 class Detector:
   def __init__(self):
-    pass
+    self.detector = None
+    if 'apriltag' in sys.modules:
+      self.detector = apriltag.Detector(searchpath=apriltag._get_demo_searchpath())
 
   def detect(self, image, return_image=False): 
-    ids, corners, centers, Hs = pyAprilTag.find(image)
-    data = []
-    for i in range(len(ids)):
-        # reversing corners since that's the order they were in for the older library
-        corners_list = corners[i].tolist()
-        corners_list.reverse()
-        data.append(Detection(ids[i],centers[i],corners_list,util.angle(corners_list)))
+    if self.detector == None:
+      ids, corners, centers, Hs = pyAprilTag.find(image)
+      data = []
+      for i in range(len(ids)):
+          # reversing corners since that's the order they were in for the older library
+          corners_list = corners[i].tolist()
+          corners_list.reverse()
+          data.append(Detection(ids[i],centers[i],corners_list,util.angle(corners_list)))
 
-    data.sort(key=lambda entry : entry.tag_id )
-    if return_image:
-        return data, image
+      data.sort(key=lambda entry : entry.tag_id )
+      if return_image:
+          return data, image
+      else:
+          return data
     else:
-        return data
+      return self.detector.detect(image, return_image)
