e993084b9a3ea16cd5c7e3d908a541b17365360b
Emily Zhang <emz37@cornell.edu>
2022-03-12 15:34:37 -0500

    fixed input forms to be on same line

--- static/gui/static/js/components/Vision/circleForm.js
+++ static/gui/static/js/components/Vision/circleForm.js
@@ -3,13 +3,19 @@ import React from 'react'
 export default function CircleForm() {
   return (
     <React.Fragment>
-      <div class="form-inline">
-      <label class="sr-only" for="x">x</label>
+      <div class="form-row">
+        <div class = "form-group col-md-3">
+        <label class="sr-only" for="x">x</label>
         <input type="text" class="form-control mb-2 mr-sm-2" id="x" placeholder="center x"/>
-      <label class="sr-only" for="y">y</label>
+        </div>
+        <div class = "form-group col-md-3">
+        <label class="sr-only" for="y">y</label>
         <input type="text" class="form-control mb-2 mr-sm-2" id="y" placeholder="center y"/> 
-      <label class="sr-only" for="r">radius</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="r" placeholder="radius"/>  */}
+        </div>
+        <div class = "form-group col-md-3">
+        <label class="sr-only" for="r">radius</label>
+        <input type="text" class="form-control mb-2 mr-sm-2" id="r" placeholder="radius"/>
+        </div>
       </div>
     </React.Fragment>
     
