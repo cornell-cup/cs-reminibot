bab84f3374872a9e79efde7eddf8410cd933183a
swathi-parthibha <swathiparthibha@gmail.com>
2022-03-12 12:51:19 -0500

    revert

--- minibot/minibot.py
+++ minibot/minibot.py
@@ -70,8 +70,6 @@ class Minibot:
         self.blockly_python_proc = BlocklyPythonProcess(BOT_LIB_FUNCS)
         signal.signal(signal.SIGINT, self.sigint_handler)
 
-        # set slave for arduino
-        ece.start_transmit()
 
     def main(self):
         """ Implements the main activity loop for the Minibot.  This activity 
