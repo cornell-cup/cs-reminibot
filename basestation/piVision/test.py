f6c7abbcf4212a473d8d8af836cc827c13acb2b9
Esther Wang <ew375@cornell.edu>
2022-03-12 14:25:31 -0500

    kind of right and left movement + combining contours

--- basestation/piVision/test.py
+++ basestation/piVision/test.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+from sqlalchemy import true
 
 # Create an object to read camera video 
 camVid = cv2.VideoCapture(0)
@@ -34,30 +35,90 @@ while(True):
     contours, hierarchy = cv2.findContours(invertedBinary, cv2.RETR_TREE,
     cv2.CHAIN_APPROX_SIMPLE)
     
-    with_contours = cv2.drawContours(output, contours, -1,(0,0,255),3)
+    # with_contours = cv2.drawContours(output, contours, -1,(0,0,255),3)
 
     #with_contours = cv2.drawContours(frame, contours, -1,(0,0,255),3)
+    for c in contours:
+      area = cv2.contourArea(c)
 
-    sumLeft = 0
-    sumRight = 0
+      if area < 10:
+        cv2.fillPoly(binary, pts=[c], color=0)
+
+    binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (51,51)))
+
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    with_contours = cv2.drawContours(frame, contours, -1, (0, 255, 0),3)
+      
 
     rows = len(binary)
-    columns = len(binary[0])
-    for i in range(rows):
-      for j in range(columns):
-        if(binary[i][j]> 0):
-          if(columns/2 - 1 > j):
-           sumLeft = sumLeft + 1
-          else:
-            sumRight = sumRight + 1
+    cols = len(binary[0])
+    middle = cols/2
 
-    cv2.putText(with_contours, fps, (650, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
+    left = 0
+    right = 0 
+
+    for con in contours:
+      rect = cv2.minAreaRect(con)
+      poly = cv2.boxPoints(rect)
+
+      valid = False
+      minX = cols
+      maxX = 0
+      minY = rows
+      maxY = 0
+      
+      for i in range(len(poly)):
+        cur = poly[i]
+        x = cur[0]
+        y = cur[1]
+        if x < minX: 
+          minX = x 
+        if x > maxX: 
+          maxX = x
+        if y < minY:
+          minY = y
+        if y > maxY:
+          maxY = y
+
+        if not (x < 10 or abs(x - cols) < 10 ) and (y < 10 or abs(y - rows) < 10 ):
+          valid = True
+
+      if valid:
+        area = cv2.contourArea(con)
+        medX = (minX + minY)/2
+        medY = (minY+maxY)/2
+
+        if(medX > middle): 
+          right += area
+        else:
+          left += area
+
+    if(left + right > 50):
+      if(left > right): 
+        print("go right")
+      else:
+        print("go left")
+
+    # print(poly)
+
+    # sumLeft = 0
+    # sumRight = 0
+
+    # rows = len(binary)
+    # columns = len(binary[0])
+    # for i in range(rows):
+    #   for j in range(columns):
+    #     if(binary[i][j]> 0):
+    #       if(columns/2 - 1 > j):
+    #        sumLeft = sumLeft + 1
+    #       else:
+    #         sumRight = sumRight + 1
+
+    # cv2.putText(with_contours, fps, (650, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
   
 
     # Display the frame, saved in the file   
     cv2.imshow('output', with_contours)
-
-
 
     # Press Q on keyboard to stop recording
     if cv2.waitKey(1) & 0xFF == ord('Q'):
@@ -65,7 +126,7 @@ while(True):
 
 # release video capture
 # and video write objects
-cap.release()
+# cap.release()
 
 # Closes all the frames
 cv2.destroyAllWindows() 
