f3067f371629fd9022167e513873069cccc0e5c1
tl676 <tl676@cornell.edu>
2022-03-12 16:01:12 -0500

    remove interim transcript for speech recognition

--- static/gui/static/js/components/utils/SpeechRecognitionComp.js
+++ static/gui/static/js/components/utils/SpeechRecognitionComp.js
@@ -30,12 +30,12 @@ function SpeechRecognitionComp({ setText, mic }) {
     }
     let finalTranscript = ''
     recognition.onresult = event => {
-      let interimTranscript = ''
+      // let interimTranscript = ''
 
       for (let i = event.resultIndex; i < event.results.length; i++) {
         const transcript = event.results[i][0].transcript;
         if (event.results[i].isFinal) finalTranscript += transcript + ' ';
-        else interimTranscript += transcript;
+        // else interimTranscript += transcript;
       }
       // document.getElementById('interim').innerHTML = interimTranscript
       // setText(interimTranscript);
