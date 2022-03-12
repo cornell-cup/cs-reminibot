ae5e7935f1ea52ff39badafde700c86f7012d983
Trenton Lyke <tjl223@cornell.edu>
2022-03-12 15:35:40 -0500

    editting forms

--- static/gui/static/js/components/Vision/circleForm.js
+++ static/gui/static/js/components/Vision/circleForm.js
@@ -4,14 +4,15 @@ export default function CircleForm() {
   return (
     <React.Fragment>
       <div class="form-inline">
-      <label class="sr-only" for="x">x</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="x" placeholder="center x"/>
-      <label class="sr-only" for="y">y</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="y" placeholder="center y"/> 
-      <label class="sr-only" for="r">radius</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="r" placeholder="radius"/>  */}
+        <label class="sr-only" for="x">x</label>
+        <input type="text" class="form-control mb-2 mr-sm-2" id="x" placeholder="center x" />
+        <label class="sr-only" for="y">y</label>
+        <input type="text" class="form-control mb-2 mr-sm-2" id="y" placeholder="center y" />
+        <label class="sr-only" for="r">radius</label>
+        <input type="text" class="form-control mb-2 mr-sm-2" id="r" placeholder="radius" />
+        <button class="btn btn-success">Submit</button>
       </div>
     </React.Fragment>
-    
+
   )
 }
