c563d8cf72cd030c039b6791915cbcfe92357226
Trenton Lyke <tjl223@cornell.edu>
2022-03-14 18:00:38 -0400

    got rid of werkseug issue

--- basestation/base_station.py
+++ basestation/base_station.py
@@ -100,7 +100,7 @@ class BaseStation:
         # only bind in debug mode if you are the debug server, if you are the
         # monitoring program which restarts the debug server, do not bind,
         # otherwise the debug server won't be able to bind
-        if app_debug and os.environ["WERKZEUG_RUN_MAIN"] == "true":
+        if app_debug and os.environ and os.environ["WERKZEUG_RUN_MAIN"] == "true":
             self.sock.bind(server_address)
         else:
             # since we are running in debug mode, always bind
