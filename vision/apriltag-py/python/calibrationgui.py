57eceb87438b6cdc095070b735466d2be83b279e
kchiem12 <kchiem12@gmail.com>
2022-03-26 16:01:56 -0400

    Merge branch 'vision_doc_updated_gui_interpolation' of https://github.com/cornell-cup/cs-reminibot into vision_doc_updated_gui_interpolation

--- vision/apriltag-py/python/calibrationgui.py
+++ vision/apriltag-py/python/calibrationgui.py
@@ -7,6 +7,8 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen, TransitionBase
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
+import part1_checkerboard as checkerboard #importing the calibration function
+
 
 class FirstWindow(Screen):
 	pass
@@ -21,10 +23,13 @@ kv = Builder.load_file('boxlayout.kv')
 #everytime in kivy, want to import something!
 
 
+
 class CalibrationApp(App):
 	def build(self):
 		Window.clearcolor = (153/255, 158/255, 154/255, 1)
 		return kv
+		
+
 
 if __name__ == '__main__':
 	CalibrationApp().run()
