eacd58e9f1cf15d7aac7d09f31c13ee591fde226
CorbinDS <cds244@cornell.edu>
2022-03-31 17:17:23 -0400

    Fixes to the program, cleaned out irrelevant code

--- basestation/piVision/server.py
+++ basestation/piVision/server.py
@@ -85,174 +85,143 @@ frame_time_queue = Queue(maxsize=5)
 
 # start looping over all the frames
 while True:
-    print("got here!!")
-    # receive RPi name and frame from the RPi and acknowledge
-    # the receipt
-    (rpiName, frame) = imageHub.recv_image()
-    imageHub.send_reply(b'OK')
+  
+  
+  # receive RPi name and frame from the RPi and acknowledge
+  # the receipt
+  (rpiName, frame) = imageHub.recv_image()
+  imageHub.send_reply(b'OK')
 
-    # if a device is not in the last active dictionary then it means
-    # that its a newly connected device
-    if rpiName not in lastActive.keys():
-        print("[INFO] receiving data from {}...".format(rpiName))
+  cv2.imshow("On-Bot Video Stream", frame)
+  # detect any kepresses
+  key = cv2.waitKey(1) & 0xFF
+
+  # if a device is not in the last active dictionary then it means
+  # that its a newly connected device
+  if rpiName not in lastActive.keys():
+    print("[INFO] receiving data from {}...".format(rpiName))
 
     # record the last active time for the device from which we just
     # received a frame
-    lastActive[rpiName] = datetime.now()
+  lastActive[rpiName] = datetime.now()
 
-    frame_count += 1
+  frame_count += 1
 
     # calculate the frame rate as the average of the last 5 frames
-    if (frame_count <= 4):
-        prev_frame_time = new_frame_time
-        new_frame_time = time.time()
-        fps = frame_count/new_frame_time
-        frame_time_queue.put(new_frame_time)
-    else:
-        prev_frame_time = new_frame_time
-        new_frame_time = time.time()
-        last_frame_time = frame_time_queue.get()
-        fps = 5/(new_frame_time - last_frame_time)
-        frame_time_queue.put(new_frame_time)
+  if (frame_count <= 4):
+    prev_frame_time = new_frame_time
+    new_frame_time = time.time()
+    fps = frame_count/new_frame_time
+    frame_time_queue.put(new_frame_time)
+  else:
+    prev_frame_time = new_frame_time
+    new_frame_time = time.time()
+    last_frame_time = frame_time_queue.get()
+    fps = 5/(new_frame_time - last_frame_time)
+    frame_time_queue.put(new_frame_time)
 
-    # converting the fps into integer
-    fps = int(fps)
+  # converting the fps into integer
+  fps = int(fps)
 
-    # converting the fps to string so that we can display it on frame
-    # by using putText function
-    fps = str(fps)
+  # converting the fps to string so that we can display it on frame
+  # by using putText function
+  fps = str(fps)
 
-    # resize the frame to have a maximum width of 400 pixels, then
-    # grab the frame dimensions and construct a blob
-    frame = imutils.resize(frame, width=700, inter=cv2.INTER_NEAREST)
-    (h, w) = frame.shape[:2]
-    if(version == 1):
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+  # resize the frame to have a maximum width of 400 pixels, then
+  # grab the frame dimensions and construct a blob
+  frame = imutils.resize(frame, width=700, inter=cv2.INTER_NEAREST)
+  (h, w) = frame.shape[:2]
+  blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
                                  0.007843, (300, 300), 127.5)
 
-        #pass the blob through the network and obtain the detections and
-        #predictions
-        net.setInput(blob)
-        detections = net.forward()
+  #pass the blob through the network and obtain the detections and
+  #predictions
+  net.setInput(blob)
+  detections = net.forward()
 
-        #reset the object count for each object in the CONSIDER set
-        objCount = {obj: 0 for obj in CONSIDER}
+  #reset the object count for each object in the CONSIDER set
+  objCount = {obj: 0 for obj in CONSIDER}
 
 
-    if(version == 2):
-        # boundaries = [
-        #     ([17, 15, 100], [50, 56, 200]),
-        #     ([86, 31, 4], [220, 88, 50]),
-        #     ([25, 146, 190], [62, 174, 250]),
-        #     ([103, 86, 65], [145, 133, 128])]
+    
+  #loop over the detections
+  for i in np.arange(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with
+    # the prediction
+    confidence = detections[0, 0, i, 2]
 
-        # for (lower, upper) in boundaries:
-        #     lower = np.array(lower, dtype = "uint8")
-        #     upper = np.array(upper, dtype = "uint8")
-        #     mask = cv2.inRange(frame, lower, upper)
-        #     output = cv2.bitwise_and(frame, frame, mask = mask)
-        #     cv2.imshow("images", np.hstack([frame, output]))
-        #     cv2.waitKey(0)
+    # filter out weak detections by ensuring the confidence is
+    # greater than the minimum confidence
+    if confidence > args["confidence"]:
+      # extract the index of the class label from the
+      # detections
+      idx = int(detections[0, 0, i, 1])
 
-        # Convert BGR to HSV
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+      # check to see if the predicted class is in the set of
+      # classes that need to be considered
+      if CLASSES[idx] in CONSIDER:
+        # increment the count of the particular object
+        # detected in the frame
+        objCount[CLASSES[idx]] += 1
 
-        # define blue color range
-        light_blue = np.array([110,50,50])
-        dark_blue = np.array([130,255,255])
+        # compute the (x, y)-coordinates of the bounding box
+        # for the object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
 
-        # Threshold the HSV image to get only blue colors
-        mask = cv2.inRange(hsv, light_blue, dark_blue)
+        # draw the bounding box around the detected object on
+        # the frame
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
 
-        # Bitwise-AND mask and original image
-        output = cv2.bitwise_and(frame,frame, mask= mask)
-
-        # Display the frame, saved in the file   
-        cv2.imshow('color detection', np.hstack([frame,output]))
-
-    if(version == 1):
-        #loop over the detections
-        for i in np.arange(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the prediction
-            confidence = detections[0, 0, i, 2]
-
-            # filter out weak detections by ensuring the confidence is
-            # greater than the minimum confidence
-            if confidence > args["confidence"]:
-                # extract the index of the class label from the
-                # detections
-                idx = int(detections[0, 0, i, 1])
-
-                # check to see if the predicted class is in the set of
-                # classes that need to be considered
-                if CLASSES[idx] in CONSIDER:
-                    # increment the count of the particular object
-                    # detected in the frame
-                    objCount[CLASSES[idx]] += 1
-
-                    # compute the (x, y)-coordinates of the bounding box
-                    # for the object
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-
-                    # draw the bounding box around the detected object on
-                    # the frame
-                    cv2.rectangle(frame, (startX, startY), (endX, endY),
-                                (255, 0, 0), 2)
-
-    # draw the sending device name on the frame
-    cv2.putText(frame, rpiName, (10, 25),
+  # draw the sending device name on the frame
+  cv2.putText(frame, rpiName, (10, 25),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-    # puting the FPS count on the frame
-    cv2.putText(frame, fps, (650, 30), cv2.FONT_HERSHEY_SIMPLEX,
+  # puting the FPS count on the frame
+  cv2.putText(frame, fps, (650, 30), cv2.FONT_HERSHEY_SIMPLEX,
                 1, (100, 255, 0), 3, cv2.LINE_AA)
 
-    if(version == 1):
-    #draw the object count on the frame
-        label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
-                        objCount.items())
-        cv2.putText(frame, label, (10, h - 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+  #draw the object count on the frame
+  label = ", ".join("{}: {}".format(obj, count) for (obj, count) inobjCount.items())
+  cv2.putText(frame, label, (10, h - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        #update the new frame in the frame dictionary
-        frameDict[rpiName] = frame
+  #update the new frame in the frame dictionary
+  frameDict[rpiName] = frame
 
-    #build a montage using images in the frame dictionary
-    # montages = build_montages(frameDict.values(), (w, h), (mW, mH))
+  #build a montage using images in the frame dictionary
+  # montages = build_montages(frameDict.values(), (w, h), (mW, mH))
 
-    #display the montage(s) on the screen
-    # for (i, montage) in enumerate(montages):
-    #     cv2.imshow("On-Bot Video Stream ({})".format(i),
-    #                montage)
-    # print(i, datetime.now())
+  #display the montage(s) on the screen
+  # for (i, montage) in enumerate(montages):
+  #     cv2.imshow("On-Bot Video Stream ({})".format(i),
+  #                montage)
+  # print(i, datetime.now())
 
-    cv2.imshow("On-Bot Video Stream", frame)
+  cv2.imshow("On-Bot Video Stream", frame)
 
     # detect any kepresses
-    key = cv2.waitKey(1) & 0xFF
+  key = cv2.waitKey(1) & 0xFF
 
 
-    if(version == 1):
-    #if current time *minus* last time when the active device check
-    #was made is greater than the threshold set then do a check
-        if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-            # loop over all previously active devices
-            for (rpiName, ts) in list(lastActive.items()):
-                # remove the RPi from the last active and frame
-                # dictionaries if the device hasn't been active recently
-                if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-                    print("[INFO] lost connection to {}".format(rpiName))
-                    lastActive.pop(rpiName)
-                    frameDict.pop(rpiName)
+  #if current time *minus* last time when the active device check
+  #was made is greater than the threshold set then do a check
+  if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
+    # loop over all previously active devices
+    for (rpiName, ts) in list(lastActive.items()):
+      # remove the RPi from the last active and frame
+      # dictionaries if the device hasn't been active recently
+      if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
+        print("[INFO] lost connection to {}".format(rpiName))
+        lastActive.pop(rpiName)
+        frameDict.pop(rpiName)
 
-            # set the last active check time as current time
-            lastActiveCheck = datetime.now()
+        # set the last active check time as current time
+      lastActiveCheck = datetime.now()
 
     # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+  if key == ord("q"):
+    break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
