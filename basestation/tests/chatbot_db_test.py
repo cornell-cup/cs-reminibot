a365346b7b26dd3d89687f753c7858d9175f7a34
tl676 <tl676@cornell.edu>
2022-04-05 22:23:12 -0400

    Add more db tests, fix backend code, edit front end SR

--- basestation/tests/chatbot_db_test.py
+++ basestation/tests/chatbot_db_test.py
@@ -139,9 +139,112 @@ def test_get_db_context(client):
     assert "the sky is green" in (returned_context).lower()
     assert "the sky is blue" in (returned_context).lower()
 
-# TODO corner cases:
-# log in without registering
-# add/get context without logging in
+
+def test_clear_local_context(client):
+    # send the clear command
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(command = 'clear')),
+        content_type='application/json'
+    )
+    # make sure local context is clear now
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(
+        command = 'get-all-local-context',
+        )),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    returned_context = data['context']
+    assert response.status_code == 200
+    assert "the sky is green" not in ' '.join(returned_context).lower()
+
+
+def test_get_db_guest(client):
+    """ Test fetch context from database if the user is not logged in. 
+    """
+    # logout
+    response = client.post(
+        '/logout/'
+    )
+    # get from database
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(command = 'get-all-db-context')),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    # returned_context = data['context']
+    assert response.status_code != 200
+    # assert "the sky is green" not in (returned_context).lower()
+    # assert "the sky is blue" in (returned_context).lower()
+    
+
+def test_commit_db_guest(client):
+    """try to commit to database if not logged in"""
+
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(command = 'commit-to-db')),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    assert response.status_code != 200
+    
+
+def test_add_local_context_guest(client):
+    """ Test that adding context locally works.
+    """
+    # change local context
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(
+            command = 'update',
+            context = "the sky is green."
+        )),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    assert response.status_code == 200
+
+
+def test_get_local_context_guest(client):
+    """ Test local context was updated.
+    """
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(
+        command = 'get-all-local-context',
+        )),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    returned_context = data['context']
+    assert response.status_code == 200
+    assert "the sky is green" in ' '.join(returned_context).lower()
+    assert "the sky is blue" in ' '.join(returned_context).lower()
+
+
+def test_clear_local_context_guest(client):
+    # send the clear command
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(command = 'clear')),
+        content_type='application/json'
+    )
+    # make sure local context is clear now
+    response = client.post(
+        '/chatbot-context',
+        data = json.dumps(dict(
+        command = 'get-all-local-context',
+        )),
+        content_type='application/json'
+    )
+    data = json.loads(response.data.decode())
+    returned_context = data['context']
+    assert response.status_code == 200
+    assert "the sky is green" not in ' '.join(returned_context).lower()
 
 
 if __name__ == "__main__":
