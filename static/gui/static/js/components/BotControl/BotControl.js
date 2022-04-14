c12f12d03d378d96df03723e76b06070065c1923
Trenton-Lyke <trentonlyke@gmail.com>
2022-04-14 12:04:46 -0400

    remove previous gridview iterations

--- static/gui/static/js/components/BotControl/BotControl.js
+++ static/gui/static/js/components/BotControl/BotControl.js
@@ -2,15 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 
 import AddBot from "./SetupBot/AddBot.js";
 import MovementControls from "./MovementControl/MovementControl.js";
-import GridView from "../Vision/gridview.js";
-import GridviewWithPhysics from "../Vision/gridviewWithPhysics.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import InformationBoxModal from "../utils/InformationBoxModal.js";
 import { INFOBOXTYPE, INFOBOXID, INFO_ICON } from "../utils/Constants.js";
 library.add(faInfoCircle);
-import VisionUserInterface from "../Vision/VisionUserInterface.js";
 import UltimateGridview from "../Vision/UltimateGridview.js";
 
 function BotControl({
