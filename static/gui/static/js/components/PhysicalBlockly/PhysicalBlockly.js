b5a5f6fc8a433f5c22d38817ad7d284594ae5496
LinJing-Liu <linjingl93@gmail.com>
2022-12-03 12:12:46 -0500

    add info modal and collapsible selections

--- static/gui/static/js/components/PhysicalBlockly/PhysicalBlockly.js
+++ static/gui/static/js/components/PhysicalBlockly/PhysicalBlockly.js
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom';
 
 import React from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
 
 import CodeMirror from 'react-codemirror';
 require('codemirror/mode/python/python');
 
+import { INFO_ICON } from '../utils/Constants.js';
 import SelectionBox from './SelectionBox';
 
 const commands = ['Move Foward', 'Move Backward', 'Turn Left', 'Turn Right', 'Stop'];
@@ -33,13 +36,13 @@ for(var i = 0; i < commands.length; i ++) {
 export default class PhysicalBlockly extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { stage: 0, tabs: 0, loopvar: 0, lastBlock: null, blockStack: [], loopList: [], code: "", customCommands: new Map(), tempCommandData: new Map(), detectionState: false, detectionCall: null, unsavedCustomization: false };
+		this.state = { stage: 0, tabs: 0, loopvar: 0, lastBlock: null, blockStack: [], loopList: [], code: "", customCommands: new Map(), tempCommandData: new Map(), detectionState: false, detectionCall: null, unsavedCustomization: false, collapsedSelection: true };
 		this.codeRef = React.createRef();
 		this.pollForUpdates = this.pollForUpdates.bind(this);
-		this.respondToTag = this.respondToTag.bind(this);
 		this.saveSelection = this.saveSelection.bind(this);
 		this.getCustomCommandID = this.getCustomCommandID.bind(this);
 		this.getPBMap = this.getPBMap.bind(this);
+		this.toggleCollapse = this.toggleCollapse.bind(this);
 		this.bWorkspace = null;
 	}
 
@@ -166,6 +169,8 @@ export default class PhysicalBlockly extends React.Component {
 				// _this.codeRef["current"].getCodeMirror().setValue(_this.props.pb);
 				_this.codeRef["current"].getCodeMirror().setValue(_this.state.code);
 
+				console.log(response.data.substring(3));
+
 				let block = document.createElement("block");
 				if (response.data.substring(3) == "for i in range(n):") {
 					isLoop = true;
@@ -267,19 +272,6 @@ export default class PhysicalBlockly extends React.Component {
 		this.setState({customCommands: newCustomCommand, unsavedCustomization: false});
     }
 
-	detectRFID() {
-		console.log("start detecting RFID");
-		console.log(this.state.customCommands);
-		this.bWorkspace.clear();
-		let detectionState = this.state.detectionState;
-		this.setState({detectionState: !this.state.detectionState});
-		if (detectionState) {
-			clearInterval(this.state.detectionCall);
-		} else {
-			this.setState({detectionCall: setInterval(this.respondToTag, 1000)});
-		}
-	}
-
 	getCustomCommandID(id) {
 		var color = choices[id];
 		let mapEntries = this.state.customCommands.entries();
@@ -292,85 +284,8 @@ export default class PhysicalBlockly extends React.Component {
 		return 0;
 	}
 
-	respondToTag() {
-		const _this = this;
-		axios.post('/start_physical_blockly', {
-			"bot_name": this.props.selectedBotName
-		})
-		.then(function (response) {
-			console.log("Received response from rfid call");
-			console.log(response);
-			console.log(response.data);
-			let commandID = tagMapping.indexOf(response.data);
-			commandID = _this.getCustomCommandID(commandID);
-
-			let isLoop = false;
-			let isEnd = false;
-			if (commandID == -1) {
-				return;
-			}
-			let n = "";
-			for (let i = 0; i < _this.state.tabs; i++) {
-				n += "    ";
-			}
-			n += commandDisplay[commandID] + "\n";
-			_this.setState({ loopvar: _this.state.loopvar + 1 });
-			_this.setState({ code: _this.state.code + n });
-			_this.codeRef["current"].getCodeMirror().setValue(_this.state.code);
-
-			let block = document.createElement("block");
-			if (commandID == 4) {
-				isEnd = true;
-			} else if (commandID == 0) {
-				block.setAttribute("type", "move_power");
-			} else if (commandID == 1) {
-				block.setAttribute("type", "move_power");
-				let dir_field = document.createElement("field");
-				dir_field.setAttribute("name", "direction");
-				dir_field.innerHTML = "backward";
-				block.appendChild(dir_field);
-			} else if (commandID == 4) {
-				block.setAttribute("type", "stop_moving");
-			} else if (commandID == 3) {
-				block.setAttribute("type", "turn_power")
-				let dir_field = document.createElement("field");
-				dir_field.setAttribute("name", "direction");
-				dir_field.innerHTML = "right"
-				block.appendChild(dir_field);
-			} else if (commandID == 2) {
-				block.setAttribute("type", "turn_power")
-				let dir_field = document.createElement("field");
-				dir_field.setAttribute("name", "direction");
-				dir_field.innerHTML = "left"
-				block.appendChild(dir_field);
-			}
-			if (block.getAttribute("type") != null) {
-				let placedBlock = Blockly.Xml.domToBlock(block, _this.bWorkspace);
-				let childConnection = placedBlock.previousConnection;
-				if (_this.state.lastBlock != null) {
-					_this.state.lastBlock.connect(childConnection);
-				}
-				if (isLoop) {
-					_this.setState({ lastBlock: placedBlock.getInput("DO").connection });
-					let tempStack = _this.state.blockStack;
-					tempStack.push(placedBlock);
-					let tempList = _this.state.loopList;
-					tempList.push(placedBlock);
-					_this.setState({ blockStack: tempStack, loopList: tempList });
-
-				}
-				else {
-					_this.setState({ lastBlock: placedBlock.nextConnection });
-				}
-			}
-			else {
-				let lastLoop = _this.state.blockStack.pop();
-				_this.setState({ blockStack: _this.state.blockStack });
-				_this.setState({ lastBlock: lastLoop.nextConnection });
-			}
-		}).catch(function (error) {
-			console.log(error);
-		});
+	toggleCollapse() {
+		this.setState({collapsedSelection: !this.state.collapsedSelection});
 	}
 
 	render(props) {
@@ -397,7 +312,8 @@ export default class PhysicalBlockly extends React.Component {
             fontSize: "22px",
             paddingBottom: "0px",
             marginTop: "15px",
-            marginBottom: "10px"
+            marginBottom: "10px",
+			marginLeft: "3px"
 		}
 
 		let warningLabelStyle = {
@@ -405,26 +321,73 @@ export default class PhysicalBlockly extends React.Component {
 			fontSize: "18px",
 			paddingBottom: "10px"
 		}
+
+		let modalBodyStyle = {
+			color: "black",
+			fontSize: "16px",
+			fontFamily: "arial"
+		}
+
 		return (
 			<div className="row">
 				<div className="col">
-					<p className="small-title"> Physical Blockly </p>
+					<p className="small-title"> Physical Blockly 
+						<span style={{ leftMargin: "0.3em" }}> </span>
+						<button className="btn"
+							data-toggle="modal"
+							data-target="#pbinfo"
+							style={{ backgroundColor: "transparent", boxShadow: "none" }}>
+								<img src={INFO_ICON} style={{ width: "18px", height: "18px" }} />
+						</button>
+
+						<div class="modal fade" id="pbinfo" tabindex="-1" role="dialog" aria-labelledby="pbInfoModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="pbInfoModalLabel">Physical Blockly Guide</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body" style={modalBodyStyle}>
+										Make sure that you are connected to a bot before continuining. 
+										You can use the toggle boxes in the customization section to select which color corresponds to which command.
+										Make sure to save your selections and then you can use start camera/live mode see the results!
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">I got this!</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</p>
 					{this.props.selectedBotName != '' && this.state.stage == 0 ?
 						<div>
-							<p className="small-title" style={customTitleStyle}> Customization of Blocks </p>
-							{
-								commands.map((c) => <SelectionBox key={c.id} command={c} choiceList={choices} default={customCommand.get(c)} pb={this} changeSelection={this.updateSelection}/> )
-							}
-							{
-								this.state.unsavedCustomization ? <div style={warningLabelStyle}>Warning: the current block customization is unsaved.</div> : <span></span>
-							}
-							<button className="btn btn-primary element-wrapper mr-1" onClick={() => this.detectRFID()}>{this.state.detectionState ? "Stop" : "Start"} Detect RFID</button>
+							<p>
+								<a class="btn" data-toggle="collapse" href="#selectionBoxCollapse" role="button" aria-expanded="false" aria-controls="selectionBoxCollapse"
+									style={{ backgroundColor: "rgb(177, 199, 255)", boxShadow: "none" }}
+									onClick={this.toggleCollapse}>
+									<FontAwesomeIcon icon={this.state.collapsedSelection ? Icons.faCaretRight : Icons.faCaretDown} />
+								</a>
+								<span className="small-title" style={customTitleStyle}> Customization of Blocks </span>
+							</p>
+							<div class="collapse" id="selectionBoxCollapse">
+								{
+									commands.map((c) => <SelectionBox key={c.id} command={c} choiceList={choices} default={customCommand.get(c)} pb={this} changeSelection={this.updateSelection}/> )
+								}
+								{
+									this.state.unsavedCustomization ? <div style={warningLabelStyle}>Warning: the current block customization is unsaved.</div> : <span></span>
+								}
+							</div>
 							<button className="btn btn-primary element-wrapper mr-1" onClick={() => this.saveSelection()}>Save</button>
 							<button className="btn btn-primary element-wrapper mr-1" onClick={() => this.physicalBlocklyClick(0)}>Start Camera Mode</button>
 							<button className="btn btn-primary element-wrapper mr-1" onClick={() => this.physicalBlocklyClick(1)}>Start Real Time Mode</button>
 						</div>
 						: this.props.selectedBotName != '' && this.state.stage == 1 ?
-						<button className="btn btn-primary element-wrapper mr-1" onClick={() => this.endProcess()}>End Process</button>
+						<div>
+							<div style={warningLabelStyle}>Please be patient as it may take a moment for the blocks to get detected.</div>
+							<button className="btn btn-primary element-wrapper mr-1" onClick={() => this.endProcess()}>End Process</button>
+						</div>
 						: <p className="white-label">Please return to Bot Control and connect to a minibot before proceeding.</p>
 					}
 					<div className='row'>
